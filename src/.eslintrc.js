5769304a41dae733658bb99ebb607096181fa8fa
Matt Howard <mwhoward@us.ibm.com>
2017-02-08 15:39:04 -0600

    Setup webpack for test framework #29

---
+++ src/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	extends: [
+		"@dap/eslint-config-portal-common",
+		"@dap/eslint-config-portal-common/react"
+	].map(require.resolve),
+    "env": {
+    },
+    rules: {
+        // Disable strict warning on ES6 Components
+        "strict": 0,
+    	"global-require": 0,
+        "sort-imports": 0,
+    	"react/jsx-indent-props": [2, "tab"]
+    }
+};
