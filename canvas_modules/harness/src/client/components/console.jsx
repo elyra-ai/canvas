a44eb0b16837c4e477cc0970e14aeea6107512ff
cou <cou@us.ibm.com>
2017-02-16 14:08:01 -0800

    add copyright header to source files #37

--- canvas_modules/harness/src/client/components/console.jsx
+++ canvas_modules/harness/src/client/components/console.jsx
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Licensed Materials - Property of IBM
+ * (c) Copyright IBM Corporation 2017. All Rights Reserved.
+ *
+ * Note to U.S. Government Users Restricted Rights:
+ * Use, duplication or disclosure restricted by GSA ADP Schedule
+ * Contract with IBM Corp.
+ *******************************************************************************/
+
 import React from "react";
 
 export default class Console extends React.Component {
--- canvas_modules/harness/src/client/constants/constants.js
+++ canvas_modules/harness/src/client/constants/constants.js
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Licensed Materials - Property of IBM
+ * (c) Copyright IBM Corporation 2017. All Rights Reserved.
+ *
+ * Note to U.S. Government Users Restricted Rights:
+ * Use, duplication or disclosure restricted by GSA ADP Schedule
+ * Contract with IBM Corp.
+ *******************************************************************************/
+
 export default {
 	SIDE_PANEL_FORMS: "sidepanel-forms",
 	SIDE_PANEL_STYLES: "sidepanel-styles",
