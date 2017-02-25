7a81edd248deb6e0dc10ab29fb15bf909390c4e7
cou <cou@us.ibm.com>
2017-02-24 16:13:07 -0800

    read common-canvas.css from npm module #50

--- canvas_modules/harness/src/client/index.js
+++ canvas_modules/harness/src/client/index.js
@@ -11,6 +11,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "../styles/index.css";
+import "@wdp/common-canvas/assets/styles/common-canvas.css";
 
 ReactDOM.render(
 	<App />,
