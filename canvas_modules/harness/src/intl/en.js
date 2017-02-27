c693804f09ca537dfa48fed4e498ea529f0cd02a
cou <cou@us.ibm.com>
2017-02-27 11:11:33 -0800

    add en.js for translation #55

---
+++ canvas_modules/harness/src/intl/en.js
@@ -0,0 +1,27 @@
+/* eslint max-len: ["error", 200] */
+export const messages = {
+	"node-context.editNode": "Open",
+	"node-context.viewModel": "View Model",
+	"node-context.connectNode": "Connect",
+	"node-context.createSuperNode": "Create supernode",
+	"node-context.expandSuperNode": "Expand supernode",
+	"node-context.disconnectNode": "Disconnect",
+	"node-context.deleteNode": "Delete",
+	"node-context.editMenu": "Edit",
+	"node-context.executeNode": "Execute",
+	"node-context.previewNode": "Preview",
+
+	"edit-context.cutSelection": "Cut",
+	"edit-context.copySelection": "Copy",
+
+	"comment-context.deleteComment": "Delete",
+
+	"link-context.deleteLink": "Delete",
+
+	"canvas-context.addComment": "New comment",
+	"canvas-context.pasteClipboard": "Paste",
+	"canvas-context.selectAll": "Select all",
+	"canvas-context.streamProperties": "Options",
+
+	"tooltip.popupPaletteButton": "Click to show node palette"
+};
