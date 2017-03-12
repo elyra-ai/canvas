3cc5d3080ef2eb990adf9fc2e299554286d9c642
Matt Howard <mwhoward@us.ibm.com>
2017-03-12 09:15:47 -0500

    Update css path

--- canvas_modules/common-canvas/src/index.js
+++ canvas_modules/common-canvas/src/index.js
@@ -1,4 +1,4 @@
-import '../assets/styles/common-canvas.css';
+import './../assets/styles/common-canvas.css';
 
 export CommonCanvas from './common-canvas.jsx';
 export ObjectModel from './object-model/object-model';
