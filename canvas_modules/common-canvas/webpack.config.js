4c97459d52a9ae0c33cad8ce61f9180fa363ea18
Matt Howard <mwhoward@us.ibm.com>
2017-03-13 08:39:45 -0500

    Update webpack config for css #66

--- canvas_modules/common-canvas/webpack.config.js
+++ canvas_modules/common-canvas/webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
         test: /\.css$/,
         loaders: [
 					"style-loader",
-					"css-loader"
+					"css-loader?localIdentName=[name]__[local]___[hash:base64:5]"
 				]
       }
 		]
