d05b7d1d50ff69ec7d0bc36872615e3d01a7cb5a
Matt Howard <mwhoward@us.ibm.com>
2017-05-16 16:06:10 -0500

    NGP-TWC/wdp-abstract-canvas#91 script changes to publish master builds to Artifactory

--- scripts/publish_modules.sh
+++ scripts/publish_modules.sh
@@ -6,42 +6,47 @@ WORKING_DIR="$PWD"
 RELEASE_BRANCH="release"
 MASTER="master"
 
-if [[ ! -z "${BUILD_USER}" && ! -z "${BUILD_USER_EMAIL}" ]]; then
-	git config --global user.name ${BUILD_USER}
-	git config --global user.email ${BUILD_USER_EMAIL}
-	echo "GIT user set as: Username: ${BUILD_USER} # Email: ${BUILD_USER_EMAIL}"
-fi
-
 # Update package.json version on master only
 if [[ ${TRAVIS_BRANCH} == ${MASTER} ]]; then
+	# In Travis the build uses a branch.  Switch to master to update package.json
+	git checkout ${MASTER}
+	git fetch origin
+	git pull
 	# Increment version in package.json for common-canvas.  Defaults to patch
 	echo "Update patch version of common-canvas"
 	cd ./canvas_modules/common-canvas
 	npm version patch
-	cd ../../
-	git commit -m "Update $UPDATE_TYPE version for common-canvas [skip ci]" ./canvas_modules/common-canvas/package.json
+	cd $WORKING_DIR
+	git status
+	git add ./canvas_modules/common-canvas/package.json
+	git commit -m "Update $UPDATE_TYPE version for common-canvas [skip ci]"
 
 	echo "Push changes to master"
 	git push origin ${MASTER}
 fi
 
+
+cd ./canvas_modules/common-canvas
+# Get the build number from the package
+BUILDNUM=`node -p "require('./package.json').version"`
+cd $WORKING_DIR
+
 # Only tag release builds
 if [[ ${TRAVIS_BRANCH} == ${RELEASE_BRANCH} ]]; then
-	cd ./canvas_modules/common-canvas
-	# Get the build number from the package
-	BUILDNUM=`node -p "require('./package.json').version"`
 	# Tag the release build before publishing
-	cd ../../scripts
+	cd ./scripts
 	./tagBuild.sh $BUILDNUM
-	cd ../
+	cd $WORKING_DIR
 fi
 
 echo "Publishing common-canvas $BUILDNUM to NPM"
 cd ./canvas_modules/common-canvas
 if [[ ${TRAVIS_BRANCH} == ${MASTER} ]]; then
-	#Update npm repository for master builds
-	sed -i '' 's|"registry":.*|"registry": "'${ARTIFACTORY_NPM_REPO}'"|g' package.json
+	# Update Artifactory npm repository for master builds
+	npm publish --registry ${ARTIFACTORY_NPM_REPO}
+elif [[ ${TRAVIS_BRANCH} == ${RELEASE_BRANCH} ]]; then
+	# Update Whitewater npm repository for master builds
+	npm publish --registry https://npm-registry.whitewater.ibm.com/
 fi
-npm publish
 
 cd $WORKING_DIR
--- scripts/setEnv.sh
+++ scripts/setEnv.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-RELEASE_BRANCH="release"
+printf "@wdp:registry=https://na.artifactory.swg-devops.com/artifactory/api/npm/analytics-canvas-npm-local/\n" > .npmrc
+printf "//na.artifactory.swg-devops.com/artifactory/api/npm/analytics-canvas-npm-local/:_password=${ARTIFACTORY_NPM_TOKEN}\n" >> .npmrc
+printf "//na.artifactory.swg-devops.com/artifactory/api/npm/analytics-canvas-npm-local/:username=${ARTIFACTORY_NPM_USERNAME}\n" >> .npmrc
+printf "//na.artifactory.swg-devops.com/artifactory/api/npm/analytics-canvas-npm-local/:email=${BUILD_USER_EMAIL}\n" >> .npmrc
+printf "//na.artifactory.swg-devops.com/artifactory/api/npm/analytics-canvas-npm-local/:always-auth=true\n" >> .npmrc
+printf "registry=https://npm-registry.whitewater.ibm.com/\n" >> .npmrc
+printf "//npm-registry.whitewater.ibm.com/:_authToken=${PRIVATE_NPM_TOKEN}\n" >> .npmrc
 
-if [[ ${TRAVIS_BRANCH} == ${RELEASE_BRANCH} ]]; then
-	printf "registry=https://npm-registry.whitewater.ibm.com/\n//npm-registry.whitewater.ibm.com/:_authToken=${PRIVATE_NPM_TOKEN}" > .npmrc
-else
-	#assume master branch
-	printf "registry=${ARTIFACTORY_NPM_REPO}\n_auth=${ARTIFACTORY_NPM_TOKEN}\nalways-auth=true\nemail=${BUILD_USER_EMAIL}" > .npmrc
-fi
 cp .npmrc ./canvas_modules/common-canvas/
 cp .npmrc ./canvas_modules/harness/
