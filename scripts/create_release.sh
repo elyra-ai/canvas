301bb6d1c3cf199500de7850b42a36d7066988d6
Matt Howard <mwhoward@us.ibm.com>
2017-05-19 16:44:08 -0500

    NGP-TWC/wml-canvas-planning#91 debugging create release script

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -4,6 +4,7 @@ set -e
 echo "Add github to known hosts"
 ssh -o StrictHostKeyChecking=no -T git@github.ibm.com
 
+echo "Setting local variables"
 WORKING_DIR="$PWD"
 SCRIPT_DIR=$(dirname "$0")
 GIT_ORG="NGP-TWC"
@@ -13,9 +14,9 @@ RELEASE_BRANCH="release"
 MASTER="master"
 GIT_USER ="Y9CTMV866"
 GIT_USER_EMAIL="Y9CTMV866@nomail.relay.ibm.com"
-
-git config --global user.name ${GIT_USER}
-git config --global user.email ${GIT_USER_EMAIL}
+echo "Done setting local variables"
+git config --global user.name "${GIT_USER}"
+git config --global user.email "${GIT_USER_EMAIL}"
 echo "GIT user set as: Username: ${GIT_USER} # Email: ${GIT_USER_EMAIL}"
 
 echo "Clone wdp-abstract-canvas"
