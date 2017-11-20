60e19eb53866ee0dc503ef1320985434f9f64fef
THOMAS FRIEDRICH <tfriedr@us.ibm.com>
2017-11-20 06:15:13 -0800

    NGP-TWC/wdp-abstract-canvas#813 improve tooltip (#823)

--- canvas_modules/common-canvas/src/tooltip.jsx
+++ canvas_modules/common-canvas/src/tooltip.jsx
@@ -10,7 +10,7 @@
 	This code is a modified version from portal-common-api for the common-canvas toolbar
 	https://github.ibm.com/dap/portal-common-api/blob/master/src/js/nav/v2/nav.js
 */
-
+/* eslint complexity: ["error", 25] */
 import React from "react";
 import PropTypes from "prop-types";
 
@@ -44,40 +44,86 @@ class ToolTip extends React.Component {
 		// set a delay on displaying the tooltip
 		if (!this.state.pendingTooltip) {
 			const that = this;
+			const delay = this.props.delay ? this.props.delay : 1000;
 			this.setState({
 				pendingTooltip: setTimeout(function() {
 					that.setTooltipVisible(true);
-				}, 1000)
+				}, delay)
 			});
 		}
 
 	}
 
 	updateTooltipLayout(tooltip, tooltipTrigger) {
-		var textWidth = tooltip.querySelector("span").offsetWidth;
-		var tooltipStyle = getComputedStyle(tooltip);
-		var horizontalPadding = parseFloat(tooltipStyle.paddingLeft) +
-				parseFloat(tooltipStyle.paddingRight);
-		var tooltipCorrection = 3;
-		var tooltipWidth = textWidth + horizontalPadding + tooltipCorrection;
-		tooltip.style.width = tooltipWidth + "px";
+		const tooltipDirection = tooltip.getAttribute("direction");
+		const tooltipWidth = tooltip.offsetWidth;
+		const tooltipHeight = tooltip.offsetHeight;
+		const viewPortWidth = document.documentElement.clientWidth;
+		const viewPortHeight = document.documentElement.clientHeight;
+		const triggerLayout = tooltipTrigger.getBoundingClientRect();
+		const pointer = tooltip.querySelector("svg");
+		const pointerLayout = pointer.getBoundingClientRect();
+		const pointerCorrection = 1;
 
-		var viewportMargin = 5;
-		var rightViewportMargin = document.documentElement.clientWidth - viewportMargin;
-		var triggerLayout = tooltipTrigger.getBoundingClientRect();
-		var tooltipLeft = Math.min(triggerLayout.left, rightViewportMargin - tooltipWidth);
-		if (tooltipWidth < triggerLayout.width) {
-			tooltipLeft += (triggerLayout.width - tooltipWidth) / 2;
+		// tooltip - left correction
+		if (tooltipDirection === "top" || tooltipDirection === "bottom") {
+			var tooltipLeft = triggerLayout.left;
+			if ((viewPortWidth - tooltipWidth) < triggerLayout.left) {
+				tooltipLeft = viewPortWidth - tooltipWidth; // hitting right border
+			} else if (tooltipWidth > triggerLayout.width) {
+				tooltipLeft -= (tooltipWidth - triggerLayout.width) / 2; // distribute overlap evenly left and right
+				if (tooltipLeft < 0) {
+					tooltipLeft = triggerLayout.left; // hitting left border
+				}
+			} else if (tooltipWidth < triggerLayout.width) {
+				tooltipLeft += (triggerLayout.width - tooltipWidth) / 2; // center tip within triggerLayout
+			}
+			tooltip.style.left = tooltipLeft + "px";
+		} else if (tooltipDirection === "left") {
+			tooltip.style.left = (triggerLayout.left - tooltipWidth - pointerLayout.width) + "px";
+		} else if (tooltipDirection === "right") {
+			tooltip.style.left = (triggerLayout.right + pointerLayout.width) + "px";
 		}
-		tooltip.style.left = Math.max(tooltipLeft, viewportMargin) + "px";
 
-		var pointer = tooltip.querySelector("svg");
-		var pointerLayout = pointer.getBoundingClientRect();
-		tooltip.style.top = (triggerLayout.bottom + pointerLayout.height) + "px";
+		// tooltip - top correction
+		if (tooltipDirection === "top") {
+			tooltip.style.top = (triggerLayout.top - pointerLayout.height - tooltipHeight) + "px";
+		} else if (tooltipDirection === "bottom") {
+			tooltip.style.top = (triggerLayout.bottom + pointerLayout.height) + "px";
+		} else if (tooltipDirection === "left" || tooltipDirection === "right") {
+			var tooltipTop = triggerLayout.top;
+			if ((viewPortHeight - tooltipHeight) < triggerLayout.top) {
+				tooltipTop = viewPortHeight - tooltipHeight; // hitting bottom border
+			} else if (tooltipHeight > triggerLayout.height) {
+				tooltipTop -= (tooltipHeight - triggerLayout.height) / 2; // distribute overlap evenly top and bottom
+				if (tooltipTop < 0) {
+					tooltipTop = triggerLayout.top; // hitting top border
+				}
+			} else if (tooltipHeight < triggerLayout.height) {
+				tooltipTop += (triggerLayout.height - tooltipHeight) / 2; // center tip within triggerLayout
+			}
+			tooltip.style.top = tooltipTop + pointerCorrection + "px";
+		}
 
-		var pointerCorrection = 1;
-		pointer.style.left = (triggerLayout.left - tooltip.getBoundingClientRect().left +
+		// pointer - left correction
+		if (tooltipDirection === "top" || tooltipDirection === "bottom") {
+			pointer.style.left = (triggerLayout.left - tooltip.getBoundingClientRect().left +
 				tooltipTrigger.offsetWidth / 2 - pointerLayout.width / 2	 + pointerCorrection) + "px";
+		} else if (tooltipDirection === "left") {
+			pointer.style.left = (tooltipWidth - 3) + "px";
+		} else if (tooltipDirection === "right") {
+			pointer.style.left = (-pointerLayout.width + 2) + "px";
+		}
+
+		// pointer - top correction
+		if (tooltipDirection === "top") {
+			pointer.style.top = (tooltipHeight - 5) + "px";
+		} else if (tooltipDirection === "bottom") {
+			pointer.style.top = "-11px";
+		} else if (tooltipDirection === "left" || tooltipDirection === "right") {
+			pointer.style.top = (triggerLayout.top - tooltip.getBoundingClientRect().top +
+				tooltipTrigger.offsetHeight / 2 - pointerLayout.height / 2) + "px";
+		}
 	}
 
 	render() {
@@ -88,19 +134,33 @@ class ToolTip extends React.Component {
 		if (this.props.disable) {
 			style.display = "none";
 		}
+		var tooltipContent = null;
+		if ((typeof this.props.tip) === "string") {
+			tooltipContent = (
+				<span id="tooltipContainer">
+					{this.props.tip}
+				</span>
+			);
+		} else if ((typeof this.props.tip) === "object") {
+			tooltipContent = (
+				<div id="tooltipContainer">
+					{this.props.tip}
+				</div>
+			);
+		}
+		const direction = this.props.direction ? this.props.direction : "bottom";
+
 		return (
 			<div>
 				<div id={this.props.id + "-trigger"} onMouseOver={mouseover} onMouseLeave={mouseleave} onMouseDown={mousedown}>
 					{this.props.children}
 				</div>
-				<div id={this.props.id} className="common-canvas_tooltip" style={style} aria-hidden={!this.state.showToolTip}>
+				<div id={this.props.id} className="common-canvas_tooltip" style={style} aria-hidden={!this.state.showToolTip} direction={direction}>
 					<svg x="0px" y="0px" viewBox="0 0 9.1 16.1">
 						<polyline points="9.1,15.7 1.4,8.1 9.1,0.5" />
 						<polygon points="8.1,16.1 0,8.1 8.1,0 8.1,1.4 1.4,8.1 8.1,14.7" />
 					</svg>
-					<span>
-						{this.props.tip}
-					</span>
+					{tooltipContent}
 				</div>
 			</div>
 		);
@@ -108,10 +168,12 @@ class ToolTip extends React.Component {
 }
 
 ToolTip.propTypes = {
-	tip: PropTypes.string.isRequired,
+	tip: PropTypes.oneOfType([PropTypes.string, PropTypes.element]).isRequired,
+	direction: PropTypes.oneOf(["left", "right", "top", "bottom"]),
 	children: PropTypes.element,
 	id: PropTypes.string.isRequired,
-	disable: PropTypes.bool
+	disable: PropTypes.bool,
+	delay: PropTypes.number
 };
 
 export default ToolTip;
