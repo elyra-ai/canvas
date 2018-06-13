35b8a192c6241ab8a7140d98bef36a9ce42d225a
Matthew W. Howard <mwhoward@us.ibm.com>
2018-06-13 13:18:45 -0500

    NGP-TWC/wdp-abstract-canvas#1670 deploy (#1689)
    * NGP-TWC/wdp-abstract-canvas#1670 testing deployment fix
    
    * NGP-TWC/wdp-abstract-canvas#1670 testing deployment fix
    
    * NGP-TWC/wdp-abstract-canvas#1670 testing deployment fix

--- scripts/bluemix_deploy.sh
+++ scripts/bluemix_deploy.sh
@@ -4,7 +4,7 @@
 # IBM Confidential
 # OCO Source Materials
 
-# (C) Copyright IBM Corp. 2017
+# (C) Copyright IBM Corp. 2017, 2018
 # The source code for this program is not published or
 # otherwise divested of its trade secrets, irrespective of
 # what has been deposited with the U.S. Copyright Office.
@@ -18,9 +18,10 @@ RELEASE="release"
 # Update package.json version on master only
 if [[ ${TRAVIS_BRANCH} == ${RELEASE} ]]; then
 	echo "Installing dap-deploy"
-	cd ./canvas_modules/harness
+	# Install dap-deploy in parent directory to prevent failure when running npm install in harness directory
 	npm install @dap/dap-deploy
+	cd ./canvas_modules/harness
 	echo "Deploying to Bluemix"
-	./node_modules/@dap/dap-deploy/scripts/deploy.sh -a api.stage1.ng.bluemix.net -o MokshaCanvasShaper -s dev -d wdp-common-canvas-dev.stage1.mybluemix.net -p ./scripts/deploy/deploy.properties
+	./../../node_modules/@dap/dap-deploy/scripts/deploy-v2.sh -a api.stage1.ng.bluemix.net -o MokshaCanvasShaper -s dev -d wdp-common-canvas-dev.stage1.mybluemix.net -p ./scripts/deploy/deploy.properties
 	cd $WORKING_DIR
 fi
