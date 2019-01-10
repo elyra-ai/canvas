b15d619b3705a9042c4d8f96dc4837a581a96713
Matthew W. Howard <mwhoward@us.ibm.com>
2019-01-10 13:39:29 -0600

    NGP-TWC/wdp-abstract-canvas#2282 Source scans are failing but not failing the build  (#2291)
    * NGP-TWC/wdp-abstract-canvas#2282 Source scans are failing but not failing the build
    
    * NGP-TWC/wdp-abstract-canvas#2282 fix script syntax
    
    * NGP-TWC/wdp-abstract-canvas#2282 test failing scripts faster
    
    * NGP-TWC/wdp-abstract-canvas#2282 revert back to old version with updated path
    
    * NGP-TWC/wdp-abstract-canvas#2282 fix path
    
    * NGP-TWC/wdp-abstract-canvas#2282 set correct tag

--- scripts/source_scan.sh
+++ scripts/source_scan.sh
@@ -4,7 +4,7 @@
 # IBM Confidential
 # OCO Source Materials
 
-# (C) Copyright IBM Corp. 2018
+# (C) Copyright IBM Corp. 2018, 2019
 # The source code for this program is not published or
 # otherwise divested of its trade secrets, irrespective of
 # what has been deposited with the U.S. Copyright Office.
@@ -15,30 +15,38 @@ RELEASE="release"
 
 if [[ ${TRAVIS_BRANCH} == ${RELEASE} ]]; then
 	set -x
+	VER_NUMBER=$TRAVIS_BUILD_NUMBER
+
+	#check if JAVA_HOME is set. We will assume Travis' default JAVA_HOME will work for the Dependency check script.
+	if [ -z "$JAVA_HOME" ] ; then
+	   echo "JAVA_HOME not set"
+	   exit 11
+	fi
+
 	#download the sonar-scanner
-	wget https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.0.3.778-linux.zip
+	wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.0.3.778-linux.zip
 	if [ ! $? -eq 0 ]; then
 	    echo "Error: Problem with downloading the sonar-scanner program"
-	    exit $?
+	    exit 1
 	fi
 
 	unzip sonar-scanner-cli-3.0.3.778-linux.zip
 	if [ ! $? -eq 0 ]; then
 	    echo "Error: Problem extracting the sonar-scanner program"
-	    exit $?
+	    exit 1
 	fi
 
 	#update scanner config
 	sed -e "s#.*sonar.host.url.*#sonar.host.url=http://9.30.122.209:9000/sonarqube/#" sonar-scanner-3.0.3.778-linux/conf/sonar-scanner.properties > /tmp/foo
 	if [ ! $? -eq 0 ]; then
 	    echo "Error: Problem updating the file, sonar-scanner.properties"
-	    exit $?
+	    exit 1
 	fi
 
 	mv /tmp/foo sonar-scanner-3.0.3.778-linux/conf/sonar-scanner.properties
 	if [ ! $? -eq 0 ]; then
 	    echo "Error: Problem moving the file, sonar-scanner.properties"
-	    exit $?
+	    exit 1
 	fi
 
 	echo "sonar.projectKey=wdpCommonCanvas" > sonar-project.properties
@@ -50,13 +58,13 @@ if [[ ${TRAVIS_BRANCH} == ${RELEASE} ]]; then
 	echo "sonar.sourceEncoding=UTF-8" >> sonar-project.properties
 	if [ ! $? -eq 0 ]; then
 	    echo "Error: Problem writing the file, sonar-scanner.properties"
-	    exit $?
+	    exit 1
 	fi
 
 	#run the scanner
 	sonar-scanner-3.0.3.778-linux/bin/sonar-scanner
 	if [ ! $? -eq 0 ]; then
 	    echo "Error: Problem while running sonar-scanner"
-	    exit $?
+	    exit 1
 	fi
 fi
