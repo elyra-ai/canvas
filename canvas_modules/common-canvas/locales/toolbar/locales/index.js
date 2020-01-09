8fda6955eac9f85daa9583b9f0678304a633b7a2
SHREYAS ARUR <sarur@us.ibm.com>
2020-01-09 09:17:31 -0800

    Sb sarur ngp twc/wml canvas planning#6096 preparing for internationalization (#2681)
    * NGP-TWC/wml-canvas-planning#6096 preparing for internationalization
    
    * NGP-TWC/wml-canvas-planning#6096 preparing for internationalization
    
    * NGP-TWC/wml-canvas-planning#6096 preparing for internationalization
    
    * NGP-TWC/wml-canvas-planning#6096 preparing for internationalization
    
    * NGP-TWC/wml-canvas-planning#6096 preparing for internationalization
    
    * NGP-TWC/wml-canvas-planning#6096 preparing for internationalization

---
+++ canvas_modules/common-canvas/locales/toolbar/locales/index.js
@@ -0,0 +1,17 @@
+const de = require("./de.json");
+const en = require("./en.json");
+const fr = require("./fr.json");
+const ja = require("./ja.json");
+const ko = require("./ko.json");
+const zhTW = require("./zhTW.json");
+const zh = require("./zh.json");
+
+module.exports = {
+	"de": de,
+	"en": en,
+	"fr": fr,
+	"ja": ja,
+	"ko": ko,
+	"zh-TW": zhTW,
+	"zh": zh
+};
