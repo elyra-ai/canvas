a3681a75b285a1def5cc2b1686e3d69eb5f7bf41
Matt Howard <mwhoward@us.ibm.com>
2020-04-21 11:52:54 -0500

    Add checkboxset to conditions text

--- canvas_modules/harness/src/client/components/common-properties-conditions.jsx
+++ canvas_modules/harness/src/client/components/common-properties-conditions.jsx
@@ -1012,7 +1012,7 @@ class CommonPropertiesComponents extends React.Component {
 				<div className="harness-conditions-documentation-panels-controls-component">
 					<h3 id="--filteredEnum" className="harness-section-subtitle">Filtered Enumeration Conditions</h3>
 					<p>The <span className="harness-highlight">enum_filter</span> condition operates upon controls
-						whose parameter is backed by an enumerated list of options. This includes radiosets
+						whose parameter is backed by an enumerated list of options. This includes radioset, checkboxset,
 						and droplist controls. When the condition is true, enum_filter conditions allow authors
 						to dynamically filter the available enumeration options based upon the state of other parameters.
 					</p>
