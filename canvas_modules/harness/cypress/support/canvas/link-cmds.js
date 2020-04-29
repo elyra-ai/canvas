e05fa8b766e366bf14df96cf626bc3dee304bdf6
Neha-Gokhale <Neha.Gokhale@ibm.com>
2020-04-28 18:32:05 -0700

    Implemented Links Unit Test using Cypress framework

--- canvas_modules/harness/cypress/support/canvas/link-cmds.js
+++ canvas_modules/harness/cypress/support/canvas/link-cmds.js
@@ -19,13 +19,11 @@ Cypress.Commands.add("getLinkFromName", (linkName) => {
 	cy.getPipeline()
 		.then((pipeline) => {
 			// Get source node id
-			cy.getNodeForLabel(nodeNames[0])
-				.then((sourceNode) => {
-					const srcNodeId = sourceNode[0].getAttribute("data-id").substring(11);
+			cy.getNodeIdForLabel(nodeNames[0])
+				.then((srcNodeId) => {
 					// Get target node id
-					cy.getNodeForLabel(nodeNames[1])
-						.then((targetNode) => {
-							const trgNodeId = targetNode[0].getAttribute("data-id").substring(11);
+					cy.getNodeIdForLabel(nodeNames[1])
+						.then((trgNodeId) => {
 							// Get canvas link from object model joining the source and target nodes
 							const canvasLink = pipeline.links.find((lnk) =>
 								lnk.srcNodeId === srcNodeId && lnk.trgNodeId === trgNodeId);
@@ -53,4 +51,62 @@ Cypress.Commands.add("clickDecoratorHotspotOnLink", (decoratorId, linkName) => {
 	cy.getLinkFromName(linkName)
 		.find(`.d3-link-dec-outline[data-id=link_dec_outln_0_${decoratorId}]`)
 		.click();
+});
+
+Cypress.Commands.add("linkNodeOutputPortToNodeInputPort", (srcNodeName, srcPortId, trgNodeName, trgPortId) => {
+	cy.getNodePortSelector(srcNodeName, "out_port", srcPortId)
+		.then((srcSelector) => {
+			cy.getNodePortSelector(trgNodeName, "inp_port", trgPortId)
+				.then((trgSelector) => {
+					// We're using { force: true } on mousemove and mouseup triggers
+					// to disable element visibility errorCheck from Cypress
+					cy.get(srcSelector)
+						.trigger("mousedown", { button: 0 });
+					cy.get(trgSelector)
+						.trigger("mousemove", { force: true })
+						.trigger("mouseup", { force: true });
+				});
+		});
+});
+
+Cypress.Commands.add("getNodePortSelector", (nodeName, nodeElement, portId) => {
+	const inst = document.extraCanvas === true ? "1" : "0";
+	cy.getNodeIdForLabel(nodeName)
+		.then((nodeId) => {
+			const portSelector = `[data-id='node_${nodeElement}_${inst}_${nodeId}_${portId}']`;
+			return portSelector;
+		});
+});
+
+Cypress.Commands.add("getPortLinks", (pipeline, srcNodeName, srcPortId, trgNodeName, trgPortId) => {
+	const links = pipeline.links;
+	cy.getNodeIdForLabel(srcNodeName)
+		.then((srcNodeId) => {
+			cy.getNodeIdForLabel(trgNodeName)
+				.then((trgNodeId) => {
+					var outLinks = [];
+					links.forEach(function(link) {
+						if (link.srcNodeId === srcNodeId &&
+							link.trgNodeId === trgNodeId &&
+							link.srcNodePortId === srcPortId &&
+							link.trgNodePortId === trgPortId) {
+							outLinks.push(link);
+						}
+					});
+					return outLinks;
+				});
+		});
+});
+
+Cypress.Commands.add("verifyLinkPath", (srcNodeName, srcPortId, trgNodeName, trgPortId, path) => {
+	cy.getPipeline()
+		.then((pipeline) => {
+			cy.getPortLinks(pipeline, srcNodeName, srcPortId, trgNodeName, trgPortId)
+				.then((links) => {
+					cy.wrap(links).should("have.length", 1);
+
+					cy.get(getLinkSelector(links[0].id, "line"))
+						.then((link) => expect(link[0].getAttribute("d")).to.equal(path));
+				});
+		});
 });
