5ca0e6d281bee1ae5fa7a2a73cd92851de5726ee
Matt Howard <mwhoward@us.ibm.com>
2020-06-12 09:11:30 -0500

    Fix copyright

--- canvas_modules/common-canvas/src/common-properties/panels/column/index.js
+++ canvas_modules/common-canvas/src/common-properties/panels/column/index.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * Licensed Materials - Property of IBM
- * (c) Copyright IBM Corporation 2018. All Rights Reserved.
+ * (c) Copyright IBM Corporation 2020. All Rights Reserved.
  *
  * Note to U.S. Government Users Restricted Rights:
  * Use, duplication or disclosure restricted by GSA ADP Schedule
