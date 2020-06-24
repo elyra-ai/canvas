476e5e6c30511699253e5c5e3f605cb48504a7c1
CTomlyn <tomlyn@us.ibm.com>
2020-06-23 18:17:08 -0700

    #141 Enable clipboard actions on the browser's Edit menu (#142)

--- canvas_modules/harness/cypress/integration/canvas/clipboard.js
+++ canvas_modules/harness/cypress/integration/canvas/clipboard.js
@@ -138,3 +138,72 @@ describe("Test for clipboard Copy and Paste context menu items", function() {
 		cy.verifyNumberOfLinks(12);
 	});
 });
+
+// ---------------------------------------------------------------------------
+// Two tests below test by simulating clicks in the clipboard actions in the
+// Browser's Edit Menu.
+// The Cypress test framework doesn't let us simulate a click on the browser's
+// menus so we send a corresponding event instead.
+// ---------------------------------------------------------------------------
+describe("Test for clipboard Cut and Paste context menu items", function() {
+	before(() => {
+		cy.visit("/");
+		cy.openCanvasDefinition("commentColorCanvas.json");
+	});
+
+	it("Test cutting some nodes and a comment and paste to canvas using browser's edit menu", function() {
+		// Validate there are 8 links on the canvas with port style
+		cy.verifyNumberOfLinks(8);
+
+		// I ctrl/cmd click the comment with text " comment 1" to select it
+		cy.ctrlOrCmdClickComment(" comment 1");
+
+		// Select multiple nodes
+		cy.ctrlOrCmdClickNode("DRUG1n");
+		cy.ctrlOrCmdClickNode("Na_to_K");
+		cy.ctrlOrCmdClickNode("Discard Fields");
+
+		// Cut nodes and comment using browser's Edit menu
+		cy.simulateClickInBrowsersEditMenu("cut");
+
+		// Paste nodes and comment using browser's Edit menu
+		cy.simulateClickInBrowsersEditMenu("paste");
+
+		cy.verifyNumberOfNodes(6);
+		cy.verifyNumberOfComments(3);
+		// There are 7 links because a data link has disappeared during the cut and paste
+		cy.verifyNumberOfLinks(7);
+	});
+});
+
+
+describe("Test for clipboard menu items on Browser Edit menu", function() {
+	before(() => {
+		cy.visit("/");
+		cy.openCanvasDefinition("commentColorCanvas.json");
+	});
+
+	it("Test copying some nodes and a comment and paste to canvas using browser's edit menu", function() {
+		// Validate there are 8 links on the canvas with port style
+		cy.verifyNumberOfLinks(8);
+
+		// I ctrl/cmd click the comment with text " comment 2" to select it
+		cy.ctrlOrCmdClickComment(" comment 2");
+
+		// Select multiple nodes
+		cy.ctrlOrCmdClickNode("Define Types");
+		cy.ctrlOrCmdClickNode("C5.0");
+		cy.ctrlOrCmdClickNode("Neural Net");
+
+		// Copy nodes and comment using browser's Edit menu
+		cy.simulateClickInBrowsersEditMenu("copy");
+
+		// Paste nodes and comment using browser's Edit menu
+		cy.simulateClickInBrowsersEditMenu("paste");
+
+		cy.verifyNumberOfNodes(9);
+		cy.verifyNumberOfComments(4);
+		// There are 12 links because 2 new data link and 2 new comment links were created during the copy and paste
+		cy.verifyNumberOfLinks(12);
+	});
+});
