c8e7fa09426d685d7d61a15aa77b9366878739fd
Matt Howard <mwhoward@us.ibm.com>
2020-08-28 11:37:40 -0500

    Add unit tests

--- canvas_modules/common-canvas/src/common-properties/panels/subtabs/subtabs.jsx
+++ canvas_modules/common-canvas/src/common-properties/panels/subtabs/subtabs.jsx
@@ -18,45 +18,53 @@ import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import { Tabs, Tab } from "carbon-components-react";
+import { getDataId } from "./../../util/control-utils";
 import { STATES } from "./../../constants/constants.js";
 
 class Subtabs extends React.Component {
-
 	constructor(props) {
 		super(props);
 		this.state = {
-			activeSubTab: 0
+			activeTabId: ""
 		};
 	}
 
-	subTabsOnClick(tabIdx) {
-		this.setState({ activeSubTab: tabIdx });
+	onClick(tabId) {
+		this.setState({ activeTabId: tabId });
 	}
 
 	render() {
 		const subTabs = [];
+		let activeTab = 0;
+		let tabIdx = 0;
 		for (let i = 0; i < this.props.tabs.length; i++) {
 			const tab = this.props.tabs[i];
-			const subPanelItems = this.props.genUIItem(i, tab.content);
+			// TODO this might not work once we don't rerender on each change
 			const panelState = this.props.controller.getPanelState({ name: tab.group });
 			if (panelState !== STATES.HIDDEN) {
+				const subPanelItems = this.props.genUIItem(i, tab.content);
+				if (this.state.activeTabId === tab.group && panelState !== STATES.DISABLED) {
+					activeTab = tabIdx;
+				}
 				subTabs.push(
 					<Tab
 						key={"subtabs.tab." + i}
 						disabled={panelState === STATES.DISABLED}
 						className="properties-subtab"
-						tabIndex={i}
+						tabIndex={tabIdx}
 						label={tab.text}
-						onClick={this.subTabsOnClick.bind(this, i)}
+						onClick={this.onClick.bind(this, tab.group)}
+						data-id={getDataId({ name: tab.group })}
 					>
 						{subPanelItems}
 					</Tab>
 				);
+				tabIdx++;
 			}
 		}
 		return (
 			<div className={classNames("properties-sub-tab-container", { vertical: !this.props.rightFlyout })}>
-				<Tabs className="properties-subtabs" selected={this.state.activeSubTab}>
+				<Tabs className="properties-subtabs" selected={activeTab}>
 					{subTabs}
 				</Tabs>
 			</div>
