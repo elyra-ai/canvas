a3290e9ef224806ac58adafbaa337587188ec358
Luciano Resende <lresende@us.ibm.com>
2020-11-23 09:57:28 -0800

    Attribute copyright notice to Elyra Authors

--- canvas_modules/common-canvas/locales/common-properties/locales/index.js
+++ canvas_modules/common-canvas/locales/common-properties/locales/index.js
@@ -1,11 +1,18 @@
-/*******************************************************************************
-
-Licensed Materials - Property of IBM
-(c) Copyright IBM Corporation 2019. All Rights Reserved.
-Note to U.S. Government Users Restricted Rights:
-Use, duplication or disclosure restricted by GSA ADP Schedule
-Contract with IBM Corp.
-*******************************************************************************/
+/*
+ * Copyright 2017-2020 Elyra Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 const de = require("./de.json");
 const en = require("./en.json");
 const es = require("./es.json");
