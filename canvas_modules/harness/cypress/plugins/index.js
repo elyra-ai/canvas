642326079c5b0bf51356a2c734c51315b75e9f71
Neha-Gokhale <Neha.Gokhale@ibm.com>
2021-01-21 17:38:39 -0800

    Added disable-gpu flag on browser launch

--- canvas_modules/harness/cypress/plugins/index.js
+++ canvas_modules/harness/cypress/plugins/index.js
@@ -26,3 +26,12 @@ module.exports = (on) => {
 		return null;
 	} });
 };
+
+module.exports = (on, config) => {
+	on("before:browser:launch", (browser, launchOptions) => {
+		if (browser.name === "chrome" && browser.isHeadless) {
+			launchOptions.args.push("--disable-gpu");
+			return launchOptions;
+		}
+	});
+};
