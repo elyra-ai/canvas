b7be7f7aeed553ef7cb065984465e71a9d640b00
CTomlyn <tomlyn@us.ibm.com>
2021-02-26 18:04:29 -0800

    #513 Add new  action to allow consumers to replace a link instead of reject it (#514)

--- canvas_modules/harness/src/client/components/sidepanel-canvas.jsx
+++ canvas_modules/harness/src/client/components/sidepanel-canvas.jsx
@@ -629,6 +629,15 @@ export default class SidePanelForms extends React.Component {
 			/>
 		</div>);
 
+		var enableLinkReplaceOnNewConnection = (<div className="harness-sidepanel-children">
+			<Toggle
+				id="selectedLinkReplaceOnNewConnection" // Set ID to corresponding field in App.js state
+				labelText="Enable Link Replace On Drop"
+				toggled={this.props.getStateValue("selectedLinkReplaceOnNewConnection")}
+				onToggle={this.setStateValue}
+			/>
+		</div>);
+
 		var enableAssocLinkCreation = (<div className="harness-sidepanel-children">
 			<Toggle
 				id="selectedAssocLinkCreation" // Set ID to corresponding field in App.js state
@@ -1277,6 +1286,8 @@ export default class SidePanelForms extends React.Component {
 					{enableInsertNodeDroppedOnLink}
 					{divider}
 					{enableHightlightNodeOnNewLinkDrag}
+					{divider}
+					{enableLinkReplaceOnNewConnection}
 					{divider}
 					{enableAssocLinkCreation}
 					{divider}
