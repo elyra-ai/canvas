eaa8a809507ca252a753f0ced38939d7c411156a
Craig Tomlyn <tomlyn@us.ibm.com>
2021-03-05 16:55:17 -0800

    Fix cypress

--- canvas_modules/harness/cypress/support/canvas/node-cmds.js
+++ canvas_modules/harness/cypress/support/canvas/node-cmds.js
@@ -44,7 +44,7 @@ Cypress.Commands.add("doubleClickLabelOnNode", (nodeLabel) => {
 Cypress.Commands.add("clickNodeLabelEditIcon", (nodeLabel) => {
 	cy.getNodeWithLabel(nodeLabel)
 		.find(".d3-node-label-edit-icon-group")
-		.last() // With horizontal format nodes, two edit icons may be on the canvas while running tests
+		// .last() // With horizontal format nodes, two edit icons may be on the canvas while running tests
 		.click();
 });
 
