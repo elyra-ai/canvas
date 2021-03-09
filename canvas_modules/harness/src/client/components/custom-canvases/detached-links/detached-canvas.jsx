f5a026ab5737de9b43e9c68d43511a056c346861
CTomlyn <tomlyn@us.ibm.com>
2021-03-08 16:10:27 -0800

    Increase max chars

--- canvas_modules/harness/src/client/components/custom-canvases/detached-links/detached-canvas.jsx
+++ canvas_modules/harness/src/client/components/custom-canvases/detached-links/detached-canvas.jsx
@@ -86,7 +86,7 @@ export default class DetachedCanvas extends React.Component {
 				labelEditable: true,
 				labelSingleLine: false,
 				labelOutline: false,
-				labelMaxCharacters: 20,
+				labelMaxCharacters: 64,
 				portRadius: 10,
 				inputPortDisplay: false,
 				outputPortRightPosX: 4,
