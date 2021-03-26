768cbf4587e188b6b33c61e5d1d757e8d8d0a580
CTomlyn <tomlyn@us.ibm.com>
2021-03-25 22:53:19 -0700

    #598 Minor supernode enhancements (#600)

--- canvas_modules/harness/src/client/components/custom-canvases/detached-links/detached-canvas.jsx
+++ canvas_modules/harness/src/client/components/custom-canvases/detached-links/detached-canvas.jsx
@@ -142,6 +142,17 @@ export default class DetachedCanvas extends React.Component {
 			this.createDecorations(data.linkIds[0]);
 		} else if (data.editType === "redo" && command.data.editType === "linkNodes") {
 			this.createDecorations(command.data.linkIds[0]);
+		} else if (data.editType === "createSuperNode" ||
+								data.editType === "redo" && command.data.editType === "createSuperNode") {
+			const newNodeProps =
+				{ label: "Local Container",
+					description: "A Data Stage Container that is local",
+					image: "/images/custom-canvases/flows/palette/icons/supernode.svg" };
+
+			this.canvasController.setNodeProperties(
+				command.supernode.id,
+				newNodeProps,
+				command.apiPipeline.pipelineId);
 		}
 	}
 
