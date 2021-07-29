027447158dcfb8837deccb28a507e35fe8aff87b
Neha Gokhale <neha.gokhale91@gmail.com>
2021-07-29 14:38:32 -0700

    #699 Side panel element alignment in common-properties (#702)

--- canvas_modules/common-canvas/src/common-properties/panels/summary/summary.jsx
+++ canvas_modules/common-canvas/src/common-properties/panels/summary/summary.jsx
@@ -305,7 +305,13 @@ class SummaryPanel extends React.Component {
 			</div>
 		</WideFlyout>) : <div />;
 		const panelClassName = this.props.panel.className ? this.props.panel.className : "";
-		const className = classNames("properties-summary-panel", "properties-control-panel", { "hide": this.props.panelState === STATES.HIDDEN }, panelClassName);
+		const className = classNames(
+			"properties-summary-panel",
+			"properties-control-panel",
+			{ "hide": this.props.panelState === STATES.HIDDEN },
+			{ "properties-control-nested-panel": this.props.panel.nestedPanel },
+			panelClassName
+		);
 		return (
 			<div className={className} disabled={this.props.panelState === STATES.DISABLED} data-id={ControlUtils.getDataId({ name: this.props.panel.id })}>
 				{flyout}
