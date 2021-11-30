c8867654b2fb93a873678c051220e83833c5c275
nabik <nabil@nabil.com>
2021-11-30 23:37:24 +0100

    #849 Testing bottonPanel and rightPanel

--- canvas_modules/common-canvas/__tests__/common-canvas/common-canvas-test.js
+++ canvas_modules/common-canvas/__tests__/common-canvas/common-canvas-test.js
@@ -67,31 +67,31 @@ describe("CommonCanvas renders correctly", () => {
 
 	it("should render one <CanvasContents/> component", () => {
 		const config = {};
-		const wrapper = createCommonCanvas(config, canvasController, {});
+		const wrapper = createCommonCanvas(config, canvasController);
 		expect(wrapper.find(CanvasContents)).to.have.length(1);
 	});
 
 	it("should render one <PaletteDialog/> component when enablePaletteLayout is set to Modal", () => {
 		const config = { enablePaletteLayout: "Modal" };
-		const wrapper = createCommonCanvas(config, canvasController, {});
+		const wrapper = createCommonCanvas(config, canvasController);
 		expect(wrapper.find(PaletteDialog)).to.have.length(1);
 	});
 
 	it("should render one <PaletteFlyout/> component when enablePaletteLayout is not specified", () => {
 		const config = {};
-		const wrapper = createCommonCanvas(config, canvasController, {});
+		const wrapper = createCommonCanvas(config, canvasController);
 		expect(wrapper.find(PaletteFlyout)).to.have.length(1);
 	});
 
 	it("should render one <PaletteFlyout/> component when enablePaletteLayout is set to Flyout", () => {
 		const config = { enablePaletteLayout: "Flyout" };
-		const wrapper = createCommonCanvas(config, canvasController, {});
+		const wrapper = createCommonCanvas(config, canvasController);
 		expect(wrapper.find(PaletteFlyout)).to.have.length(1);
 	});
 
 	it("should not render any <PaletteDialog/> component when enablePaletteLayout is not specified", () => {
 		const config = {};
-		const wrapper = createCommonCanvas(config, canvasController, {});
+		const wrapper = createCommonCanvas(config, canvasController);
 		expect(wrapper.find(PaletteDialog)).to.have.length(0);
 	});
 
@@ -104,7 +104,7 @@ describe("CommonCanvas renders correctly", () => {
 
 	it("should render one <Toolbar/> component when there is no toolbarConfig", () => {
 		const config = {};
-		const wrapper = createCommonCanvas(config, canvasController, {});
+		const wrapper = createCommonCanvas(config, canvasController);
 		expect(wrapper.find(Toolbar)).to.have.length(1);
 	});
 
@@ -200,8 +200,8 @@ describe("CommonCanvas renders correctly", () => {
 
 		const beforeEditActionHandler = (data) => null; // Return null to stop command being executed
 		const editActionHandler = sinon.spy();
-		const canvasParams = {};
-		createCommonCanvas(config, canvasController, canvasParams, toolbarConfig, notificationConfig,
+
+		createCommonCanvas(config, canvasController, toolbarConfig, notificationConfig,
 			{ editActionHandler: editActionHandler,
 				beforeEditActionHandler: beforeEditActionHandler });
 
@@ -221,6 +221,7 @@ function createCommonCanvas(config, canvasController, canvasParams, toolbarConfi
 	const selectionChangeHandler = sinon.spy();
 	const tipHandler = sinon.spy();
 	const contextMenuConfig = null;
+	const param2 = canvasParams || {};
 	const wrapper = createIntlCommonCanvas(
 		config,
 		contextMenuHandler,
@@ -230,8 +231,8 @@ function createCommonCanvas(config, canvasController, canvasParams, toolbarConfi
 		decorationActionHandler,
 		selectionChangeHandler,
 		tipHandler,
-		canvasParams.showBottomPanel,
-		canvasParams.showRightFlyout,
+		param2.showBottomPanel,
+		param2.showRightFlyout,
 		toolbarConfig,
 		notificationConfig,
 		contextMenuConfig,
