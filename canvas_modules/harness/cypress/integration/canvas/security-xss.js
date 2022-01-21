729a2fe5fbebaa8190e2523b3f4c235b62da9021
Craig Tomlyn <tomlyn@us.ibm.com>
2022-01-20 22:01:54 -0800

    Fix comments

--- canvas_modules/harness/cypress/integration/canvas/security-xss.js
+++ canvas_modules/harness/cypress/integration/canvas/security-xss.js
@@ -20,9 +20,18 @@ describe("Test whether cross site scripting is possible", function() {
 
 	it("Test adding nodes from palette, link nodes, link comment to node, delete node, delete comment", function() {
 
+		// This will help detect if an alert has been displayed.
 		const stub = cy.stub();
 		cy.on("window:alert", stub);
 
+		// The canvas securityXss.json contains a comment, node label and text decoration
+		// that all contain the following as text "<img src onerror=alert(origin)>"
+		// If this HTML text has not been escaped properly in any of these elements
+		// it will cause an alert to be displayed. This would indicate the possibility
+		// for a XSS attack. Therefore, we check to make sure the stub is NOT called
+		// which indicates that the alert was NOT displayed. Note: Cypress doesn't
+		// actually display alerts so this is the way to detect if one would have been
+		// displayed.
 		cy.openCanvasDefinition("securityXss.json")
 			.then(() => {
 				expect(stub).to.not.be.called;
@@ -32,6 +41,9 @@ describe("Test whether cross site scripting is possible", function() {
 
 		cy.rightClickToDisplayContextMenu(400, 100);
 		cy.clickOptionFromContextMenu("New comment");
+
+		// After entering the HTML into the comment verify that an alert would
+		// NOT have been displayed on the screen.
 		cy.editTextInComment("", "<img src onerror=alert(origin)> 1")
 			.then(() => {
 				expect(stub).to.not.be.called;
