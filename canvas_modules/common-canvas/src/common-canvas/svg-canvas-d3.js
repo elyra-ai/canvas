0bd843f8ba8b14a13ba7e526f46eeeb5e208ca1a
Craig Tomlyn <tomlyn@us.ibm.com>
2022-03-18 13:55:03 -0700

    #965 Copy and paste lead to performance issues

--- canvas_modules/common-canvas/src/common-canvas/svg-canvas-d3.js
+++ canvas_modules/common-canvas/src/common-canvas/svg-canvas-d3.js
@@ -19,42 +19,17 @@
 import * as d3 from "d3-selection";
 import { cloneDeep } from "lodash";
 import SVGCanvasRenderer from "./svg-canvas-renderer.js";
-import CanvasUtils from "./common-canvas-utils.js";
 import ConfigUtils from "../object-model/config-utils.js";
 import Logger from "../logging/canvas-logger.js";
-
-const BACKSPACE_KEY = 8;
-const DELETE_KEY = 46;
-const A_KEY = 65;
-const C_KEY = 67;
-const P_KEY = 80;
-const V_KEY = 86;
-const X_KEY = 88;
-const Y_KEY = 89;
-const Z_KEY = 90;
-// TODO - Implement nudge behavior for moving nodes and comments
-// const LEFT_ARROW_KEY = 37;
-// const UP_ARROW_KEY = 38;
-// const RIGHT_ARROW_KEY = 39;
-// const DOWN_ARROW_KEY = 40;
 
 export default class SVGCanvasD3 {
 
 	constructor(canvasInfo, canvasDivSelector, config, canvasController) {
 		this.logger = new Logger(["SVGCanvasD3", "FlowId", canvasInfo.id]);
 		this.logger.logStartTimer("constructor");
-		this.mousePos = {
-			x: 0,
-			y: 0
-		};
 		this.canvasController = canvasController;
-		this.canvasDiv = this.initializeCanvasDiv(canvasDivSelector);
+		this.canvasDiv = d3.select(canvasDivSelector);
 		this.logger.logEndTimer("constructor", true);
-		document.addEventListener("mousemove", this.onMouseUpdate.bind(this), true);
-	}
-
-	close() {
-		document.removeEventListener("mousemove", this.onMouseUpdate, true);
 	}
 
 	setCanvasInfo(canvasInfo, config) {
@@ -112,88 +87,18 @@ export default class SVGCanvasD3 {
 		return cloneDeep(canvasInfo);
 	}
 
-	// Records in mousePos the mouse pointer position when the pointer is inside
-	// the boundaries of the canvas or sets the mousePos to null. This position
-	// info can be used with keyboard operations.
-	onMouseUpdate(e) {
-		if (e.target.className.baseVal === "svg-area" || e.target.className.baseVal === "d3-svg-background") {
-			this.mousePos = {
-				x: e.clientX,
-				y: e.clientY
-			};
-		} else {
-			this.mousePos = null;
-		}
+	isEditingText() {
+		return this.renderer.isEditingText();
 	}
 
-	initializeCanvasDiv(canvasDivSelector) {
+	isDragging() {
+		return this.renderer.isDragging();
+	}
 
-		// Add a listener to canvas div to catch key presses. The containing
-		// canvas div must have tabindex set and the focus set on the div.
-		const canvasDiv = d3.select(canvasDivSelector)
-			.on("keydown", (d3Event) => {
-				// Make sure no tip is displayed, because having one displayed
-				// will interfere with drawing of the canvas as the result of any
-				// keyboard action.
-				this.canvasController.closeTip();
-				const actions = this.canvasController.getKeyboardConfig().actions;
-				// We don't handle key presses when:
-				// 1. We are editng text, because the text area needs to receive key
-				//    presses for undo, redo, delete etc.
-				// 2. Dragging objects
-				if (this.renderer.isEditingText() ||
-						this.renderer.isDragging()) {
-					return;
-				}
-
-				// These actions alter the canvas objects so we need to check
-				// this.config.enableEditingActions before calling them.
-				if (this.config.enableEditingActions) {
-					if ((d3Event.keyCode === BACKSPACE_KEY || d3Event.keyCode === DELETE_KEY) && actions.delete) {
-						CanvasUtils.stopPropagationAndPreventDefault(d3Event); // Some browsers interpret Delete as 'Back to previous page'. So prevent that.
-						this.canvasController.keyboardActionHandler("deleteSelectedObjects");
-
-					} else if (CanvasUtils.isCmndCtrlPressed(d3Event) &&
-							!d3Event.shiftKey && d3Event.keyCode === Z_KEY && actions.undo) {
-						CanvasUtils.stopPropagationAndPreventDefault(d3Event);
-						this.canvasController.keyboardActionHandler("undo");
-
-					} else if (CanvasUtils.isCmndCtrlPressed(d3Event) &&
-							((d3Event.shiftKey && d3Event.keyCode === Z_KEY) || d3Event.keyCode === Y_KEY && actions.redo)) {
-						CanvasUtils.stopPropagationAndPreventDefault(d3Event);
-						this.canvasController.keyboardActionHandler("redo");
-
-					} else if (CanvasUtils.isCmndCtrlPressed(d3Event) && d3Event.keyCode === C_KEY && actions.copyToClipboard) {
-						CanvasUtils.stopPropagationAndPreventDefault(d3Event);
-						this.canvasController.keyboardActionHandler("copy");
-
-					} else if (CanvasUtils.isCmndCtrlPressed(d3Event) && d3Event.keyCode === X_KEY && actions.cutToClipboard) {
-						CanvasUtils.stopPropagationAndPreventDefault(d3Event);
-						this.canvasController.keyboardActionHandler("cut");
-
-					} else if (CanvasUtils.isCmndCtrlPressed(d3Event) && d3Event.keyCode === V_KEY && actions.pasteFromClipboard) {
-						CanvasUtils.stopPropagationAndPreventDefault(d3Event);
-						if (this.mousePos) {
-							this.mousePos = this.renderer.convertPageCoordsToCanvasCoords(this.mousePos.x, this.mousePos.y);
-							this.mousePos = this.renderer.getMousePosSnapToGrid(this.mousePos);
-							this.canvasController.keyboardActionHandler("paste", this.mousePos);
-						} else {
-							this.canvasController.keyboardActionHandler("paste");
-						}
-					}
-				}
-				// These last two keyboard actions do not alter the canvas objects so we
-				// do not need to check this.config.enableEditingActions before calling them.
-				if (CanvasUtils.isCmndCtrlPressed(d3Event) && d3Event.keyCode === A_KEY && actions.selectAll) {
-					CanvasUtils.stopPropagationAndPreventDefault(d3Event);
-					this.canvasController.keyboardActionHandler("selectAll");
-
-				} else if (CanvasUtils.isCmndCtrlPressed(d3Event) && d3Event.shiftKey && d3Event.altKey && d3Event.keyCode === P_KEY) {
-					CanvasUtils.stopPropagationAndPreventDefault(d3Event);
-					Logger.switchLoggingState(); // Switch the logging on and off
-				}
-			});
-		return canvasDiv;
+	convertPageCoordsToSnappedCanvasCoords(pos) {
+		let positon = this.renderer.convertPageCoordsToCanvasCoords(pos.x, pos.y);
+		positon = this.renderer.getMousePosSnapToGrid(positon);
+		return positon;
 	}
 
 	nodeTemplateDraggedOver(nodeTemplate, x, y) {
