c496985017c460b32b6efd7f30a0d5f7c7aa30f6
Matt Howard <mwhoward@us.ibm.com>
2022-03-24 21:18:39 -0500

    Fix field picker

--- canvas_modules/common-canvas/src/common-properties/components/field-picker/field-picker.jsx
+++ canvas_modules/common-canvas/src/common-properties/components/field-picker/field-picker.jsx
@@ -140,43 +140,40 @@ export default class FieldPicker extends React.Component {
 		for (let i = 0; i < fields.length; i++) {
 			const field = fields[i];
 			const columns = [];
-			let fieldContent = (
+			let dmIcon = null;
+			if (this.props.dmIcon && this.props.dmIcon !== "type") {
+				const metadata = this.props.controller.getDatasetMetadataFields();
+				const dmIconType = PropertyUtils.getDMFieldIcon(metadata, field.origName, this.props.dmIcon);
+				const icon = dmIconType ? <Icon type={dmIconType} /> : null;
+				// don't show icon for type since it's already being displayed
+				if (icon) {
+					dmIcon = (
+						<div className="properties-fp-field-type-icon">
+							{icon}
+						</div>
+					);
+				}
+			}
+			const fpFieldName = (
+				<span className="properties-fp-field-name">
+					{field.origName}
+				</span>
+			);
+
+			const fieldNameWithTooltip = (
+				<TruncatedContentTooltip
+					content={fpFieldName}
+					tooltipText={field.origName}
+					disabled={false}
+				/>
+			);
+			const fieldContent = (
 				<div className="properties-fp-field">
-					<div className="properties-fp-field-name">
-						{field.origName}
-					</div>
+					{dmIcon}
+					{fieldNameWithTooltip}
 				</div>
 			);
-			if (this.props.dmIcon) {
-				const metadata = this.props.controller.getDatasetMetadataFields();
-				const dmIconType = PropertyUtils.getDMFieldIcon(metadata,
-					field.origName, this.props.dmIcon);
-				const dmIcon = dmIconType ? <Icon type={dmIconType} /> : null;
-				let disabled = true;
-				if (field.origName) {
-					disabled = false;
-				}
-				const fpFieldName = (
-					<span className="properties-fp-field-name">
-						{field.origName}
-					</span>
-				);
-				const fieldNameWithTooltip = (
-					<TruncatedContentTooltip
-						content={fpFieldName}
-						tooltipText={field.origName}
-						disabled={disabled}
-					/>
-				);
-				fieldContent = (
-					<div className="properties-fp-field">
-						<div className="properties-fp-field-type-icon">
-							{dmIcon}
-						</div>
-						{fieldNameWithTooltip}
-					</div>
-				);
-			}
+
 			columns.push({
 				column: "fieldName",
 				content: fieldContent,
