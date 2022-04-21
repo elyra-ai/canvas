887203e531ab20fd646d3f618d264e5c29f2ab7e
Craig Tomlyn <tomlyn@us.ibm.com>
2022-04-21 16:15:19 -0700

    Fix tests

--- canvas_modules/harness/cypress/integration/canvas/nodes-resize.js
+++ canvas_modules/harness/cypress/integration/canvas/nodes-resize.js
@@ -28,32 +28,7 @@ describe("Test adding resizing nodes", function() {
 
 	it("Test all attached and detached links appear OK", function() {
 		cy.verifyNumberOfPortDataLinks(8);
-
-		// Test the two links from Node 2 to Node 1
-		cy.verifyLinkPath("Node 2", "outPort1", "Node 1", "inPort",
-			"M 242.630126953125 132.52344837411354 L 435.2596130371094 228.94234778966086");
-		cy.verifyLinkPath("Node 2", "outPort2", "Node 1", "inPort",
-			"M 237.00137409101524 188.49662691354752 L 435.2596130371094 305.67435851625555");
-
-		// Test the one link from Node 1 to Node 3
-		cy.verifyLinkPath("Node 1", "outPort", "Node 3", "inPort",
-			"M 435.2596130371094 414.5440487350059 L 153.09522247314453 398.32003960221454");
-
-		// Test the one link from Node 1 to Node 4
-		cy.verifyLinkPath("Node 1", "outPort", "Node 4", "inPort",
-			"M 538.2596130371094 391.7122543839917 L 713.8162978100015 303.78745532835455");
-
-		// Test the two detached links pointing away from Node 1
-		cy.verifyDetachedLinkPathFromSource("Node 1", "outPort", 2, [
-			"M 538.2596130371094 260.9572649119533 L 728 134",
-			"M 435.2596130371094 610.3088228810263 L 338 667"
-		]);
-
-		// Test the two detached links pointing at Node 1
-		cy.verifyDetachedLinkPathToTarget("Node 1", "inPort", 2, [
-			"M 764 644 L 538.2596130371094 558.9883811067392",
-			"M 158 644 L 435.2596130371094 521.6279149026254"
-		]);
+		verifyOnOpen();
 	});
 
 	it("Test all attached and detached links appear OK after moving nodes around", function() {
@@ -127,52 +102,61 @@ describe("Test adding resizing nodes", function() {
 
 	});
 
-	it("Test all attached and detached links appear OK after sizing nodes and moving detached links", function() {
+	it("Test undoing resize returns everything to original state", function() {
 		cy.verifyNumberOfPortDataLinks(8);
 
-		// Resize the central node and surrounding nodes
+		// Verify everything is OK initially
+		verifyOnOpen();
+
+		// Resize the central node
 		cy.resizeNode("Node 1", "north-east", 80, 600);
-		cy.resizeNode("Node 2", "north-east", 80, 80);
-		cy.resizeNode("Node 3", "north-east", 250, 80);
-
-		// Verify the nodes sized OK
+		// Verify the node sized OK
 		cy.verifyNodeDimensions("Node 1", 80, 600);
-		cy.verifyNodeDimensions("Node 2", 80, 80);
-		cy.verifyNodeDimensions("Node 3", 250, 80);
+		// Undo using toolbar
+		cy.clickToolbarUndo();
 
-		// Move a couple of the detached links
-		cy.clickLink("5da9a38f-41cb-4ec5-8c31-e47c6c8f4837");
-		cy.moveLinkHandleToPos("5da9a38f-41cb-4ec5-8c31-e47c6c8f4837", "startHandle", 750, 200);
+		// Verify everything is back as it was.
+		verifyOnOpen();
 
-		cy.clickLink("fb14a01d-0c83-4b4c-b5f3-5d897ba96513");
-		cy.moveLinkHandleToPos("fb14a01d-0c83-4b4c-b5f3-5d897ba96513", "endHandle", 650, 600);
-
-		// Test the two links from Node 2 to Node 1
-		cy.verifyLinkPath("Node 2", "outPort1", "Node 1", "inPort",
-			"M 132.630126953125 134.39426327077814 L 435.2596130371094 175.0799836573891");
-		cy.verifyLinkPath("Node 2", "outPort2", "Node 1", "inPort",
-			"M 132.630126953125 172.21114936658887 L 435.2596130371094 284.0937381126622");
-
-		// Test the one link from Node 1 to Node 3
-		cy.verifyLinkPath("Node 1", "outPort", "Node 3", "inPort",
-			"M 435.2596130371094 433.05223203848436 L 333.09522247314453 458.0749152887199");
-
-		// Test the one link from Node 1 to Node 4
-		cy.verifyLinkPath("Node 1", "outPort", "Node 4", "inPort",
-			"M 529.2596130371094 397.6953910845787 L 713.8162978100015 304.0610133485292");
-
-		// Test the two detached links pointing away from Node 1
-		cy.verifyDetachedLinkPathFromSource("Node 1", "outPort", 2, [
-			"M 529.2596130371094 172.33328100951485 L 728 134",
-			"M 529.2596130371094 557.9718316322422 L 650 600"
-		]);
-
-		// Test the two detached links pointing at Node 1
-		cy.verifyDetachedLinkPathToTarget("Node 1", "inPort", 2, [
-			"M 750 200 L 529.2596130371094 283.6573946546683",
-			"M 158 644 L 435.2596130371094 556.4524656210139"
-		]);
-
+		// Undo using toolbar
+		cy.clickToolbarRedo();
+		// Verify the node sized OK
+		cy.verifyNodeDimensions("Node 1", 80, 600);
 
 	});
 });
+
+// Tests the node sizes and link paths when the canvas is first opened.
+function verifyOnOpen() {
+	// Test node dimensions
+	cy.verifyNodeDimensions("Node 1", 89, 488.35507065057755);
+	cy.verifyNodeDimensions("Node 2", 190.32456970214844, 150.16434144973755);
+	cy.verifyNodeDimensions("Node 3", 70, 268.2066955566406);
+
+	// Test the two links from Node 2 to Node 1
+	cy.verifyLinkPath("Node 2", "outPort1", "Node 1", "inPort",
+		"M 242.630126953125 132.52344837411354 L 435.2596130371094 228.94234778966086");
+	cy.verifyLinkPath("Node 2", "outPort2", "Node 1", "inPort",
+		"M 237.00137409101524 188.49662691354752 L 435.2596130371094 305.67435851625555");
+
+	// Test the one link from Node 1 to Node 3
+	cy.verifyLinkPath("Node 1", "outPort", "Node 3", "inPort",
+		"M 435.2596130371094 414.5440487350059 L 153.09522247314453 398.32003960221454");
+
+	// Test the one link from Node 1 to Node 4
+	cy.verifyLinkPath("Node 1", "outPort", "Node 4", "inPort",
+		"M 538.2596130371094 391.7122543839917 L 713.8162978100015 303.78745532835455");
+
+	// Test the two detached links pointing away from Node 1
+	cy.verifyDetachedLinkPathFromSource("Node 1", "outPort", 2, [
+		"M 538.2596130371094 260.9572649119533 L 728 134",
+		"M 435.2596130371094 610.3088228810263 L 338 667"
+	]);
+
+	// Test the two detached links pointing at Node 1
+	cy.verifyDetachedLinkPathToTarget("Node 1", "inPort", 2, [
+		"M 764 644 L 538.2596130371094 558.9883811067392",
+		"M 158 644 L 435.2596130371094 521.6279149026254"
+	]);
+
+}
