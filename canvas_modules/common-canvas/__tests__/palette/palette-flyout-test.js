47ac6cacd92a850165cb40e4f95822ac12cfa127
nabik <nabil@nabil.com>
2022-05-10 18:34:28 +0100

    #1024 Hover state for palette dropdown aren't Carbon compliant

--- canvas_modules/common-canvas/__tests__/palette/palette-flyout-test.js
+++ canvas_modules/common-canvas/__tests__/palette/palette-flyout-test.js
@@ -81,11 +81,11 @@ describe("Palette search renders correctly", () => {
 
 		simulateSearchEntry(searchInput, "t");
 		wrapper.update();
-		expect(wrapper.find(PaletteContentListItem)).to.have.length(0);
+		expect(wrapper.find(PaletteContentListItem)).to.have.length(1);
 
 		simulateSearchEntry(searchInput, "test");
 		wrapper.update();
-		expect(wrapper.find(PaletteContentListItem)).to.have.length(0);
+		expect(wrapper.find(PaletteContentListItem)).to.have.length(1);
 
 	});
 
@@ -162,8 +162,8 @@ describe("Palette renders correctly", () => {
 		const wrapper = createMountedPalette();
 		const importCat = findCategoryElement(wrapper, "Import");
 		importCat.simulate("click");
-		expect(wrapper.find(PaletteFlyoutContentList)).to.have.length(1);
-		expect(wrapper.find(PaletteContentListItem)).to.have.length(3);
+		expect(wrapper.find(PaletteFlyoutContentList)).to.have.length(2);
+		expect(wrapper.find(PaletteContentListItem)).to.have.length(5);
 		const outputsCat = findCategoryElement(wrapper, "Outputs");
 		outputsCat.simulate("click");
 		expect(wrapper.find(PaletteFlyoutContentList)).to.have.length(2);
@@ -176,10 +176,14 @@ describe("Palette renders correctly", () => {
 		importCat.simulate("click");
 		const outputsCat = findCategoryElement(wrapper, "Outputs");
 		outputsCat.simulate("click");
+		// We now click the Import category again to close it
 		const importCat2 = findCategoryElement(wrapper, "Import");
 		importCat2.simulate("click");
-		expect(wrapper.find(PaletteFlyoutContentList)).to.have.length(1);
-		expect(wrapper.find(PaletteContentListItem)).to.have.length(2);
+		// When the Import category is closed and the Outputs category is opened
+		// we should have 2 palettes flyout content lists objects because
+		// content lists always exist whether categories are opened or closed.
+		expect(wrapper.find(PaletteFlyoutContentList)).to.have.length(2);
+		expect(wrapper.find(PaletteContentListItem)).to.have.length(5);
 	});
 
 	// WARNING: The data-id attribute is used by host application "walk-me"
@@ -238,7 +242,7 @@ describe("Palette renders correctly", () => {
 		const category = findCategoryElement(palette, "Category1");
 		category.simulate("click");
 		// 2 nodes should be rendered
-		expect(palette.find(PaletteContentListItem)).to.have.length(2);
+		expect(palette.find(PaletteContentListItem)).to.have.length(3);
 	});
 
 	it("narrow palette should show correct values for category and node with and without an image", () => {
@@ -256,7 +260,7 @@ describe("Palette renders correctly", () => {
 		expect(category.find("span").text()).to.equal("Cat");
 		category.simulate("click");
 		// 2 nodes should be rendered
-		expect(palette.find(PaletteContentListItem)).to.have.length(2);
+		expect(palette.find(PaletteContentListItem)).to.have.length(3);
 		// Category2 should show image when provided
 		const category2 = findCategoryElement(palette, "Category2");
 		expect(category2.find("img")).to.have.length(1);
--- canvas_modules/common-canvas/src/palette/palette-flyout-content-category.jsx
+++ canvas_modules/common-canvas/src/palette/palette-flyout-content-category.jsx
@@ -17,10 +17,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { InlineLoading } from "carbon-components-react";
-import Icon from "../icons/icon.jsx";
 import SVG from "react-inlinesvg";
-import { TIP_TYPE_PALETTE_CATEGORY, CANVAS_CARBON_ICONS } from "../common-canvas/constants/canvas-constants.js";
+import { TIP_TYPE_PALETTE_CATEGORY } from "../common-canvas/constants/canvas-constants.js";
 import { get } from "lodash";
+import { AccordionItem } from "carbon-components-react";
+import PaletteContentList from "./palette-content-list.jsx";
+
 
 class PaletteFlyoutContentCategory extends React.Component {
 	constructor(props) {
@@ -82,18 +84,6 @@ class PaletteFlyoutContentCategory extends React.Component {
 	}
 
 	getRenderCategory() {
-		let caretClassName = "palette-flyout-category-caret";
-		if (!this.props.isPaletteOpen) {
-			caretClassName += " palette-flyout-category-caret-closed"; // When palette is closed extra style
-		}
-
-		let caretImage = null;
-		if (this.props.itemCount > 0 || this.props.category.empty_text) {
-			caretImage = this.props.isCategorySelected
-				? <Icon type={CANVAS_CARBON_ICONS.CHEVRONARROWS.UP} className={caretClassName} />
-				: <Icon type={CANVAS_CARBON_ICONS.CHEVRONARROWS.DOWN} className={caretClassName} />;
-		}
-
 		let itemImage = null;
 		if (this.props.category.image && this.props.category.image !== "") {
 			if (this.props.category.image.endsWith(".svg")) {
@@ -131,8 +121,8 @@ class PaletteFlyoutContentCategory extends React.Component {
 				</span>
 			);
 		}
-
-		const content = (
+		const nodeTypeInfos = this.props.category.node_types.map((nt) => ({ nodeType: nt, category: this.props.category }));
+		const titleObj = (
 			<div className="palette-flyout-category"
 				data-id={get(this.props.category, "id", "")}
 				onClick={this.categorySelected}
@@ -143,11 +133,21 @@ class PaletteFlyoutContentCategory extends React.Component {
 				<div className="palette-flyout-category-item">
 					{itemImage}
 					{itemText}
-				</div>
-				{caretImage}
-			</div>
+				</div></div>
 		);
-		return content;
+		return (
+			<AccordionItem title={titleObj}>
+				<PaletteContentList
+					key={this.props.category.label + "-nodes"}
+					show
+					category={this.props.category}
+					nodeTypeInfos={nodeTypeInfos}
+					canvasController={this.props.canvasController}
+					isPaletteOpen={this.props.isPaletteOpen}
+					isEditingEnabled
+				/>
+			</AccordionItem>
+		);
 	}
 
 	categorySelected() {
