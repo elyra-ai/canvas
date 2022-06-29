0e0fcd5eb0c3349e5373c95cb7c027e5f7527b5a
nabik <nabil@nabil.com>
2022-06-29 14:19:58 -0700

    Adjust more errors

--- canvas_modules/common-canvas/src/common-properties/controls/radioset/radioset.jsx
+++ canvas_modules/common-canvas/src/common-properties/controls/radioset/radioset.jsx
@@ -201,7 +201,7 @@ class RadiosetControl extends React.Component {
 				row: i
 			};
 			buttons.push(
-				<div key={i} className="properties-radioset-panel">
+				<div key={i} className={`properties-radioset-panel ${Number(this.props.control.values[i]) && "properties-radioset-panel-number"}`}>
 					<RadioButton
 						key={i}
 						id={ControlUtils.getControlId(id, this.uuid)}
