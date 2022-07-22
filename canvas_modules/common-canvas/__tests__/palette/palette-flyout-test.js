00c48fc3b70de4a2d72499332be02218cbb0d005
CTomlyn <tomlyn@us.ibm.com>
2022-07-22 12:41:38 -0700

    #1125 Remove icons from palette when category is closed

--- canvas_modules/common-canvas/__tests__/palette/palette-flyout-test.js
+++ canvas_modules/common-canvas/__tests__/palette/palette-flyout-test.js
@@ -175,8 +175,8 @@ describe("Palette renders correctly", () => {
 		const wrapper = createMountedPalette();
 		const importCat = findCategoryElement(wrapper, "Import");
 		importCat.simulate("click");
-		expect(wrapper.find(PaletteFlyoutContentList)).to.have.length(2);
-		expect(wrapper.find(PaletteContentListItem)).to.have.length(5);
+		expect(wrapper.find(PaletteFlyoutContentList)).to.have.length(1);
+		expect(wrapper.find(PaletteContentListItem)).to.have.length(3);
 
 		const counts = getOpenCategories(wrapper);
 		expect(counts).to.have.length(1);
@@ -197,16 +197,20 @@ describe("Palette renders correctly", () => {
 		const outputsCat = findCategoryElement(wrapper, "Outputs");
 		outputsCat.simulate("click");
 
+		expect(wrapper.find(PaletteFlyoutContentList)).to.have.length(2);
+		expect(wrapper.find(PaletteContentListItem)).to.have.length(5);
 		const counts = getOpenCategories(wrapper);
 		expect(counts).to.have.length(2);
+
 		// We now click the Import category again to close it
 		const importCat2 = findCategoryElement(wrapper, "Import");
 		importCat2.simulate("click");
+
 		// When the Import category is closed and the Outputs category is opened
-		// we should have 2 palettes flyout content lists objects because
-		// content lists always exist whether categories are opened or closed.
-		expect(wrapper.find(PaletteFlyoutContentList)).to.have.length(2);
-		expect(wrapper.find(PaletteContentListItem)).to.have.length(5);
+		// we should have 1 palette flyout content list object because
+		// content lists are removed when a category is closed.
+		expect(wrapper.find(PaletteFlyoutContentList)).to.have.length(1);
+		expect(wrapper.find(PaletteContentListItem)).to.have.length(2);
 
 		const counts2 = getOpenCategories(wrapper);
 		expect(counts2).to.have.length(1);
@@ -265,9 +269,10 @@ describe("Palette renders correctly", () => {
 		const palette = createMountedPalette(config);
 		const categories = palette.find(PaletteFlyoutContentCategory);
 		expect(categories).to.have.length(2);
+
 		const category = findCategoryElement(palette, "Category1");
 		category.simulate("click");
-		expect(palette.find(PaletteContentListItem)).to.have.length(3);
+		expect(palette.find(PaletteContentListItem)).to.have.length(2);
 	});
 
 	it("narrow palette should show correct values for category and node with and without an image", () => {
@@ -277,14 +282,17 @@ describe("Palette renders correctly", () => {
 			paletteWidth: 64
 		};
 		const palette = createMountedPalette(config);
+
 		// 2 categories should be rendered
 		const categories = palette.find(PaletteFlyoutContentCategory);
 		expect(categories).to.have.length(2);
+
 		// Category1 should show `Cat` when no image provided
 		const category = findCategoryElement(palette, "Category1");
 		expect(category.find("span").text()).to.equal("Cat");
 		category.simulate("click");
-		expect(palette.find(PaletteContentListItem)).to.have.length(3);
+		expect(palette.find(PaletteContentListItem)).to.have.length(2);
+
 		// Category2 should show image when provided
 		const category2 = findCategoryElement(palette, "Category2");
 		expect(category2.find("img")).to.have.length(1);
