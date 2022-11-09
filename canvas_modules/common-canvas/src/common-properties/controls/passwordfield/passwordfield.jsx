1a128c6b4a4269a783dc64505727dd4c2c91151c
nabik <nabil@nabil.com>
2022-11-09 19:18:00 +0100

    #1242 Support tooltip in password control
    Signed-off-by: nabik <nabil@nabil.com>

--- canvas_modules/common-canvas/src/common-properties/controls/passwordfield/passwordfield.jsx
+++ canvas_modules/common-canvas/src/common-properties/controls/passwordfield/passwordfield.jsx
@@ -20,9 +20,9 @@ import { connect } from "react-redux";
 import { PasswordInput, Form } from "carbon-components-react";
 import ValidationMessage from "./../../components/validation-message";
 import * as ControlUtils from "./../../util/control-utils";
-import { STATES } from "./../../constants/constants.js";
+import { STATES, MESSAGE_KEYS } from "./../../constants/constants.js";
 import classNames from "classnames";
-
+import { formatMessage } from "./../../util/property-utils";
 class PasswordControl extends React.Component {
 	constructor(props) {
 		super(props);
@@ -34,6 +34,15 @@ class PasswordControl extends React.Component {
 	}
 
 	render() {
+		const overrideShowPasswordLabel = `${this.props.control.name}.password.showPasswordLabel.tooltip`;
+		const overrideHidePasswordLabel = `${this.props.control.name}.password.hidePasswordLabel.tooltip`;
+
+		const defaultShowLabel = formatMessage(this.reactIntl, MESSAGE_KEYS.SHOW_PASSWORD_TOOLTIP);
+		const defaultHidelabel = formatMessage(this.reactIntl, MESSAGE_KEYS.HIDE_PASSWORD_TOOLTIP);
+
+		const showPassword = this.props.controller.getResource(overrideShowPasswordLabel, defaultShowLabel);
+		const hidePassword = this.props.controller.getResource(overrideHidePasswordLabel, defaultHidelabel);
+
 		const value = this.props.value ? this.props.value : "";
 		const className = classNames("properties-pwdfield", "properties-input-control", { "hide": this.props.state === STATES.HIDDEN },
 			this.props.messageInfo ? this.props.messageInfo.type : null);
@@ -53,6 +62,8 @@ class PasswordControl extends React.Component {
 						hideLabel={this.props.tableControl}
 						light={this.props.controller.getLight() && !this.props.control.light}
 						tooltipAlignment="end"
+						showPasswordLabel={showPassword}
+						hidePasswordLabel={hidePassword}
 					/>
 				</Form>
 				<ValidationMessage inTable={this.props.tableControl} tableOnly state={this.props.state} messageInfo={this.props.messageInfo} />
