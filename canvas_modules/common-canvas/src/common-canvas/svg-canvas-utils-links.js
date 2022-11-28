167f8c653b401bac1fc839363736de702aef3389
CTomlyn <tomlyn@us.ibm.com>
2022-11-28 13:43:23 -0800

    #1268 Performance of resize of nodes with multiple link lines should … (#1273)
    Signed-off-by: CTomlyn <tomlyn@us.ibm.com>

--- canvas_modules/common-canvas/src/common-canvas/svg-canvas-utils-links.js
+++ canvas_modules/common-canvas/src/common-canvas/svg-canvas-utils-links.js
@@ -87,7 +87,11 @@ export default class SvgCanvasLinks {
 	// Returns the lineArray passed in with connection path info added to it.
 	addConnectionPaths(links) {
 		links.forEach((link) => {
-			link.pathInfo = this.getConnectorPathInfo(link);
+			// Only necessary to get the path info, if the start and end coords of
+			// the link have changed.
+			if (link.coordsUpdated) {
+				link.pathInfo = this.getConnectorPathInfo(link);
+			}
 		});
 		return links;
 	}
