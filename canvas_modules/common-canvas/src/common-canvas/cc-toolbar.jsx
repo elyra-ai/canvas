240a8e5bf9186035745ba2448a61386aa9c0bc29
Sara Akhtar <sara.akhtar@ibm.com>
2023-06-06 12:03:40 -0700

    small fix
    Signed-off-by: Sara Akhtar <sara.akhtar@ibm.com>

--- canvas_modules/common-canvas/src/common-canvas/cc-toolbar.jsx
+++ canvas_modules/common-canvas/src/common-canvas/cc-toolbar.jsx
@@ -177,6 +177,7 @@ class CommonCanvasToolbar extends React.Component {
 
 	optionallyAddNotificationTool(rightBar) {
 		if (this.props.notificationConfig &&
+			this.props.notificationConfig.enable !== false &&
 			typeof this.props.notificationConfig.action !== "undefined" &&
 			typeof this.props.notificationConfig.enable !== "undefined") {
 			const notificationCount = this.props.notificationMessages.length;
