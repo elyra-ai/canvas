efb5932a6121b6650985677e7e92ac2ad42789e3
Neha Gokhale <neha.gokhale@ibm.com>
2024-03-22 11:22:41 -0700

    Removed ibm/plex and review comments
    Signed-off-by: Neha Gokhale <neha.gokhale@ibm.com>

--- canvas_modules/harness/Gruntfile.js
+++ canvas_modules/harness/Gruntfile.js
@@ -61,15 +61,6 @@ module.exports = function(grunt) {
 					dest: ".build"
 				}]
 			},
-			fonts: {
-				files: [{
-					expand: true,
-					flatten: false,
-					cwd: "./node_modules/@ibm/plex",
-					src: ["IBM-Plex*/**"],
-					dest: ".build/fonts"
-				}]
-			},
 			styleguide: {
 				files: [{
 					expand: true,
@@ -124,7 +115,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks("grunt-contrib-copy");
 	grunt.registerTask("lint", ["eslint", "jsonlint", "yamllint"]);
 
-	var buildTasks = ["clean", "lint", "sass", "copy:graphics", "copy:styleguide", "copy:fonts"];
+	var buildTasks = ["clean", "lint", "sass", "copy:graphics", "copy:styleguide"];
 	if (IS_PRODUCTION) {
 		buildTasks = buildTasks.concat(["webpack"]);
 	}
