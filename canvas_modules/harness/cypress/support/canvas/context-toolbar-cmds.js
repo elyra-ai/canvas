799878c74d7321b576cbe56094df14e1c89d0851
CTomlyn <tomlyn@us.ibm.com>
2024-05-21 10:46:12 -0700

    #1968 Canvas background context toolbar should disappear when mouse c… (#1969)

---
+++ canvas_modules/harness/cypress/support/canvas/context-toolbar-cmds.js
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2024 Elyra Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+Cypress.Commands.add("clickContextToolbarOverflowButton", () => {
+	cy.getContextToolbarOverflowItem().click();
+});
+
+Cypress.Commands.add("getContextToolbarOverflowItem", () => {
+	cy.findOverflowItem(cy.getContextToolbar());
+});
+
+Cypress.Commands.add("clickContextToolbarButton", (optionName) => {
+	cy.getOptionFromContextToolbar(optionName).click();
+});
+
+Cypress.Commands.add("getOptionFromContextToolbar", (optionName) => {
+	cy.getContextToolbar()
+		.find(".toolbar-item." + optionName + "-action")
+		.first();
+});
+
+Cypress.Commands.add("clickOptionFromContextToolbarOverflow", (optionName) => {
+	cy.getOptionFromContextToolbarOverflow(optionName).click();
+});
+
+Cypress.Commands.add("getOptionFromContextToolbarOverflow", (optionName) => {
+	cy.getContextToolbar()
+		.find(".toolbar-popover-list")
+		.find(".toolbar-sub-menu-item")
+		.then((options) => {
+			for (let idx = 0; idx < options.length; idx++) {
+				if (options[idx].outerText === optionName) {
+					return options[idx];
+				}
+			}
+			return null;
+		});
+});
+
+Cypress.Commands.add("moveOutOfContextToolbar", (xPos, yPos) => {
+	// Note: react uses mouseout even though cc-context-toolbar is listening to mouseleave.
+	cy.get(".context-toolbar")
+		.trigger("mouseout", xPos, yPos, { force: true });
+});
+
+Cypress.Commands.add("getContextToolbar", () => {
+	cy.get(".context-toolbar");
+});
+
