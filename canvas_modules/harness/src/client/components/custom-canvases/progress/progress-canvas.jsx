b338bad524247ebb1b77e341b8c9a6cb2810e9e1
CTomlyn <tomlyn@us.ibm.com>
2024-07-01 10:22:18 -0700

    Files
    Signed-off-by: CTomlyn <tomlyn@us.ibm.com>

---
+++ canvas_modules/harness/src/client/components/custom-canvases/progress/progress-canvas.jsx
@@ -0,0 +1,262 @@
+/*
+ * Copyright 2017-2023 Elyra Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import { CommonCanvas, CanvasController } from "common-canvas"; // eslint-disable-line import/no-unresolved
+import { Play, StopFilledAlt } from "@carbon/react/icons";
+
+import ProgressFlow from "./progress-flow.json";
+import ProgressPalette from "../../../../../test_resources/palettes/modelerPalette.json";
+
+const nodeAnimation =
+	"animation-duration:1000ms; animation-name:wiggle2; " +
+	"animation-iteration-count:infinite; fill: skyblue;";
+
+const nodeStyle = {
+	body: { default: nodeAnimation, hover: "fill: orange; stroke: coralred; stroke-width: 5;" },
+	// selection_outline: { default: animation },
+	image: { default: null },
+	label: { default: "fill: blue" },
+	text: { default: "fill: white" }
+};
+
+const removeNodeStyle = {
+	body: { default: null, hover: null },
+	// selection_outline: { default: animation },
+	image: { default: null },
+	label: { default: null },
+	text: { default: null }
+};
+
+const nodeCompleteDec = [{
+	id: "done",
+	position: "topCenter",
+	x_pos: -10,
+	y_pos: -25,
+	height: 20,
+	width: 20,
+	outline: false,
+	image: "/images/decorators/checkmark--filled.svg",
+	temporary: true
+}];
+
+const linkAnimation =
+	"animation-duration:1000ms; animation-name:blink; " +
+	"animation-iteration-count:infinite; animation-direction: alternate";
+
+const linkStyle = {
+	line: { default: linkAnimation, hover: "stroke: yellow; stroke-width: 2" }
+};
+
+const removeLinkStyle = {
+	line: { default: null, hover: null }
+};
+
+export default class ProgressCanvas extends React.Component {
+	constructor(props) {
+		super(props);
+		this.canvasController = new CanvasController();
+		this.canvasController.setPipelineFlow(ProgressFlow);
+		this.canvasController.setPipelineFlowPalette(ProgressPalette);
+
+		this.getConfig = this.getConfig.bind(this);
+		this.runProgress = this.runProgress.bind(this);
+		this.editActionHandler = this.editActionHandler.bind(this);
+	}
+
+	getConfig() {
+		const config = Object.assign({}, this.props.config, {
+			enableParentClass: "progress",
+			enableNodeFormatType: "Vertical",
+			enableMarkdownInComments: true,
+			tipConfig: {
+				palette: true,
+				nodes: true,
+				ports: false,
+				links: false
+			}
+		});
+		return config;
+	}
+
+	getToolbarConfig() {
+		const toolbarConfig = [
+			{ action: "palette", label: "Palette", enable: true },
+			{ divider: true },
+			{ action: "runit", label: "Run", enable: true, incLabelWithIcon: "before", iconEnabled: (<Play size={32} />), kind: "primary" },
+			{ divider: true },
+			{ action: "stopit", label: "Stop", enable: true, incLabelWithIcon: "before", iconEnabled: (<StopFilledAlt size={32} />) },
+			{ divider: true },
+			{ action: "undo", label: "Undo", enable: true },
+			{ action: "redo", label: "Redo", enable: true },
+			{ action: "cut", label: "Cut", enable: true, tooltip: "Cut from clipboard" },
+			{ action: "copy", label: "Copy", enable: true, tooltip: "Copy from clipboard" },
+			{ action: "paste", label: "Paste", enable: true, tooltip: "Paste to canvas" },
+			{ action: "createAutoComment", label: "Add Comment", enable: true },
+			{ action: "deleteSelectedObjects", label: "Delete", enable: true }
+		];
+
+		return toolbarConfig;
+	}
+
+	editActionHandler(data, command, inExtraCanvas) {
+		switch (data.editType) {
+		case "runit": {
+			this.runProgress();
+			break;
+		}
+		case "stopit": {
+			this.stopProgress();
+			break;
+		}
+		default: {
+			// Do nothing
+		}
+		}
+	}
+
+	runProgress() {
+		// Note: The pipelineId uses special characters for testing purposes.
+		const pipelineId = "`~!@#$%^&*()_+=-{}][|:;<,>.9?/";
+
+		const bindingEntryNode = "id8I6RH2V91XW";
+		const executionNode = "|:;<,>.9?/`~!@#$%^&*()_+=-{}]["; // The executiion node id uses special characters for testing.
+		const superNode = "nodeIDSuperNodePE";
+		const modelNode = "id125TTEEIK7V";
+		const bindingExitNode = "id5KIRGGJ3FYT";
+
+		this.objects1 = [];
+		this.objects2 = [];
+		this.objects3 = [];
+		this.objects4 = [];
+
+		this.objects1[pipelineId] = [bindingEntryNode];
+		this.objects2[pipelineId] = [executionNode];
+		this.objects3[pipelineId] = [superNode];
+		this.objects4[pipelineId] = [modelNode, bindingExitNode];
+
+		const lnk1 = this.canvasController.getNodeDataLinkFromInfo(bindingEntryNode, "outPort", executionNode, "inPort");
+		const lnk2 = this.canvasController.getNodeDataLinkFromInfo(executionNode, null, superNode, "input2SuperNodePE");
+		const lnk3 = this.canvasController.getNodeDataLinkFromInfo(superNode, null, modelNode, "inPort");
+		const lnk4 = this.canvasController.getNodeDataLinkFromInfo(superNode, "output1SuperNodePE", bindingExitNode, "inPort");
+
+		this.link1 = [];
+		this.link2 = [];
+		this.link3 = [];
+
+		this.link1[pipelineId] = [lnk1.id];
+		this.link2[pipelineId] = [lnk2.id];
+		this.link3[pipelineId] = [lnk3.id, lnk4.id];
+
+		const that = this;
+
+		// On run, first clear all node decorations
+		this.clearNodeDecorations();
+
+		// Now begin displaying progress indication using setTimeout to
+		// simulate the passage of time.
+		that.canvasController.setObjectsStyle(this.objects1, nodeStyle, true);
+
+		this.part1 = setTimeout(() => {
+			that.canvasController.setLinksStyle(this.link1, linkStyle, true);
+			that.canvasController.setObjectsStyle(this.objects2, nodeStyle, true);
+		}, 2000);
+
+		this.part2 = setTimeout(() => {
+			that.canvasController.setObjectsStyle(this.objects1, removeNodeStyle, true);
+			that.canvasController.setNodeDecorations(bindingEntryNode, nodeCompleteDec);
+			that.canvasController.setLinksStyle(this.link1, removeLinkStyle, true);
+		}, 4000);
+
+		this.part3 = setTimeout(() => {
+			that.canvasController.setLinksStyle(this.link2, linkStyle, true);
+			that.canvasController.setObjectsStyle(this.objects3, nodeStyle, true);
+		}, 6000);
+
+		this.part4 = setTimeout(() => {
+			that.canvasController.setObjectsStyle(this.objects2, removeNodeStyle, true);
+			that.canvasController.setNodeDecorations(executionNode, nodeCompleteDec);
+			that.canvasController.setLinksStyle(this.link2, removeLinkStyle, true);
+		}, 8000);
+
+		this.part5 = setTimeout(() => {
+			that.canvasController.setLinksStyle(this.link3, linkStyle, true);
+			that.canvasController.setObjectsStyle(this.objects4, nodeStyle, true);
+		}, 10000);
+
+		this.part6 = setTimeout(() => {
+			that.canvasController.setLinksStyle(this.link3, removeLinkStyle, true);
+			that.canvasController.setObjectsStyle(this.objects3, removeNodeStyle, true);
+			that.canvasController.setNodeDecorations(superNode, nodeCompleteDec);
+		}, 12000);
+
+		this.part7 = setTimeout(() => {
+			that.canvasController.setObjectsStyle(this.objects4, removeNodeStyle, true);
+			that.canvasController.setNodeDecorations(modelNode, nodeCompleteDec);
+			that.canvasController.setNodeDecorations(bindingExitNode, nodeCompleteDec);
+		}, 14000);
+	}
+
+	stopProgress() {
+		// Stop the annimations
+		clearTimeout(this.part1);
+		clearTimeout(this.part2);
+		clearTimeout(this.part3);
+		clearTimeout(this.part4);
+		clearTimeout(this.part5);
+		clearTimeout(this.part6);
+		clearTimeout(this.part7);
+
+		// Clear any current node decorations
+		this.clearNodeDecorations();
+
+		// Clear any current node styles
+		this.canvasController.setObjectsStyle(this.objects1, removeNodeStyle, true);
+		this.canvasController.setObjectsStyle(this.objects2, removeNodeStyle, true);
+		this.canvasController.setObjectsStyle(this.objects3, removeNodeStyle, true);
+		this.canvasController.setObjectsStyle(this.objects4, removeNodeStyle, true);
+
+		// Clear any current link styles
+		this.canvasController.setLinksStyle(this.link1, removeLinkStyle, true);
+		this.canvasController.setLinksStyle(this.link2, removeLinkStyle, true);
+		this.canvasController.setLinksStyle(this.link3, removeLinkStyle, true);
+	}
+
+	clearNodeDecorations() {
+		this.canvasController.getNodes().forEach((n) => {
+			this.canvasController.setNodeDecorations(n.id, []);
+		});
+	}
+
+	render() {
+		const config = this.getConfig();
+		const toolbarConfig = this.getToolbarConfig();
+		return (
+			<CommonCanvas
+				canvasController={this.canvasController}
+				editActionHandler={this.editActionHandler}
+				toolbarConfig={toolbarConfig}
+				config={config}
+			/>
+		);
+	}
+}
+
+ProgressCanvas.propTypes = {
+	config: PropTypes.object
+};
---
+++ canvas_modules/harness/src/client/components/custom-canvases/react-nodes-carbon/react-nodes-carbon-canvas.jsx
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2023 Elyra Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import { CommonCanvas, CanvasController } from "common-canvas"; // eslint-disable-line import/no-unresolved
+
+import CardNodeWrapper from "./wrapper-card-node.jsx";
+import ShapeNodeWrapper from "./wrapper-shape-node.jsx";
+
+import ReactNodesFlow from "./react-nodes-carbon-flow.json";
+import ReactNodesPalette from "./react-nodes-carbon-palette.json";
+
+
+export default class ReactNodesCarbonCanvas extends React.Component {
+	constructor(props) {
+		super(props);
+		this.canvasController = new CanvasController();
+		this.canvasController.setPipelineFlow(ReactNodesFlow);
+		this.canvasController.setPipelineFlowPalette(ReactNodesPalette);
+
+		this.getConfig = this.getConfig.bind(this);
+		this.layoutHandler = this.layoutHandler.bind(this);
+	}
+
+	getConfig() {
+		const config = Object.assign({}, this.props.config, {
+			enableParentClass: "react-nodes-carbon",
+			enableNodeFormatType: "Vertical",
+			enableLinkType: "Curve",
+			enableLinkDirection: "LeftRight",
+			enableSnapToGridType: "After",
+			enableLinkSelection: "None",
+			enableLinkReplaceOnNewConnection: true,
+			paletteInitialState: true,
+			enableResizableNodes: true,
+			enableMarkdownInComments: true,
+			enableDropZoneOnExternalDrag: true,
+			enableContextToolbar: true,
+			enableHighlightNodeOnNewLinkDrag: true,
+			tipConfig: {
+				palette: true,
+				nodes: true,
+				ports: false,
+				links: false
+			},
+			enableNodeLayout: {
+				drawNodeLinkLineFromTo: "node_center",
+				drawCommentLinkLineTo: "node_center",
+				nodeShapeDisplay: false,
+				nodeExternalObject: CardNodeWrapper,
+				defaultNodeWidth: 220,
+				defaultNodeHeight: 100,
+				contextToolbarPosition: "topRight",
+				ellipsisPosition: "topRight",
+				ellipsisWidth: 30,
+				ellipsisHeight: 30,
+				ellipsisPosY: -35,
+				ellipsisPosX: -30,
+				imageDisplay: false,
+				labelDisplay: false,
+
+				inputPortObject: "image",
+				inputPortImage: "/images/custom-canvases/flows/decorations/dragStateArrow.svg",
+				inputPortWidth: 20,
+				inputPortHeight: 20,
+
+				outputPortRightPosX: 5,
+				outputPortRightPosY: 30,
+				outputPortObject: "image",
+				outputPortImage: "/images/custom-canvases/flows/decorations/dragStateArrow.svg",
+				outputPortWidth: 20,
+				outputPortHeight: 20,
+				outputPortGuideObject: "image",
+				outputPortGuideImage: "/images/custom-canvases/flows/decorations/dragStateArrow.svg"
+			},
+			enableCanvasLayout: {
+				dataLinkArrowHead: true,
+				linkGap: 4,
+				supernodeTopAreaHeight: 28,
+				supernodeSVGAreaPadding: 5
+			}
+		});
+		return config;
+	}
+
+	getCanvasController() {
+		return this.canvasController;
+	}
+
+	layoutHandler(node) {
+		if (node.op === "shape-node") {
+			const config = {
+				selectionPath: "M -4 -4 h 36 v 36 h -36 Z",
+				nodeExternalObject: ShapeNodeWrapper,
+				defaultNodeWidth: 28,
+				defaultNodeHeight: 70,
+				className: "shape-node",
+				contextToolbarPosition: "topCenter",
+				inputPortDisplay: false,
+				inputPortLeftPosX: -12,
+				inputPortLeftPosY: 15,
+				outputPortRightPosX: 12,
+				outputPortRightPosY: 15
+			};
+			return config;
+		}
+		return null;
+	}
+
+	render() {
+		const config = this.getConfig();
+		return (
+			<CommonCanvas
+				canvasController={this.canvasController}
+				config={config}
+				layoutHandler={this.layoutHandler}
+			/>
+		);
+	}
+}
+
+ReactNodesCarbonCanvas.propTypes = {
+	config: PropTypes.object
+};
