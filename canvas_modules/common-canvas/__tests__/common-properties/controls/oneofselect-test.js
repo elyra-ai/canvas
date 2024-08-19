27d6bafc680e17f5ee5b96de85a443dd3e3ce52f
srikant <srksriks123@gmail.com>
2024-08-19 16:43:55 +0530

    #2108 Need shouldFilterItem support for oneofselect control
    Signed-off-by: srikant <srksriks123@gmail.com>

--- canvas_modules/common-canvas/__tests__/common-properties/controls/oneofselect-test.js
+++ canvas_modules/common-canvas/__tests__/common-properties/controls/oneofselect-test.js
@@ -522,9 +522,28 @@ describe("oneofselect with custom value allowed works correctly", () => {
 		let dropdownWrapper = wrapper.find("div[data-id='properties-oneofselect-custom']");
 		const dropdownInput = dropdownWrapper.find("input");
 		dropdownInput.simulate("click");
+		// Enter 'e' in input to check [one, three] are filtered using shouldFilterItem
+		dropdownInput.simulate("change", { target: { value: "e" } });
 		dropdownWrapper = wrapper.find("div[data-id='properties-oneofselect-custom']");
 		const dropdownList = dropdownWrapper.find("li.cds--list-box__menu-item");
 		expect(dropdownList).to.be.length(2);
+	});
+
+	it("Validate oneofselect filters correctly using shouldFilterItem", () => {
+		const wrapper = mount(
+			<OneofselectControl
+				store={controller.getStore()}
+				control={control}
+				controller={controller}
+				propertyId={propertyId}
+			/>
+		);
+		let dropdownWrapper = wrapper.find("div[data-id='properties-oneofselect-custom']");
+		const dropdownInput = dropdownWrapper.find("input");
+		dropdownInput.simulate("change", { target: { value: "one" } });
+		dropdownWrapper = wrapper.find("div[data-id='properties-oneofselect-custom']");
+		const dropdownList = dropdownWrapper.find("li.cds--list-box__menu-item");
+		expect(dropdownList).to.be.length(1);
 	});
 });
 
