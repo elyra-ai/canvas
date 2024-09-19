3d6e3a9e8659075b9edf975df81c5407a42581f5
srikant <srksriks123@gmail.com>
2024-09-19 10:50:01 +0530

    Set properties-small-custom under overrideSize condition
    Signed-off-by: srikant <srksriks123@gmail.com>

--- canvas_modules/common-canvas/src/common-properties/properties-main/properties-main.jsx
+++ canvas_modules/common-canvas/src/common-properties/properties-main/properties-main.jsx
@@ -571,14 +571,7 @@ class PropertiesMain extends React.Component {
 				</PropertiesModal>);
 			}
 
-			// If Custom width is set adjust classname to format action buttons
-			const customWidth = this.propertiesController.getForm().pixelWidth;
 			let propertiesSizeClassname = `properties-${this.state.editorSize}`;
-			if (customWidth) {
-				if (this.state.editorSize === Size.SMALL) {
-					propertiesSizeClassname = "properties-small-custom";
-				}
-			}
 			const className = classNames("properties-wrapper",
 				{
 					"properties-right-flyout": this.props.propertiesConfig.rightFlyout,
@@ -590,6 +583,8 @@ class PropertiesMain extends React.Component {
 			let overrideStyle = null;
 			if (overrideSize !== null) {
 				overrideStyle = { width: overrideSize + "px" };
+				// If Custom width is set adjust classname to format action buttons
+				propertiesSizeClassname = "properties-small-custom";
 			}
 			return (
 				<Provider store={this.propertiesController.getStore()}>
