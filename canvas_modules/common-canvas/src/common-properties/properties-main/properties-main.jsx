c57c18d920239d5f111d4a1af68a235f9710c35d
srikant <srksriks123@gmail.com>
2024-09-24 21:59:53 +0530

    Edit comment for custom classname
    Signed-off-by: srikant <srksriks123@gmail.com>

--- canvas_modules/common-canvas/src/common-properties/properties-main/properties-main.jsx
+++ canvas_modules/common-canvas/src/common-properties/properties-main/properties-main.jsx
@@ -576,7 +576,7 @@ class PropertiesMain extends React.Component {
 			const overrideSize = this._getOverrideSize();
 			let overrideStyle = null;
 			if (overrideSize !== null) {
-				// Add custom classname when editor size is small so that title-editor styling is not applied
+				// Add custom classname when custom editor size is set
 				propertiesSizeClassname = "properties-custom-size";
 				overrideStyle = { width: overrideSize + "px" };
 			}
