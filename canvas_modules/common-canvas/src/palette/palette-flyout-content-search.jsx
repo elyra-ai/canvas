36149b8668b5420300e045ea9250f574bd262ccd
Prince Sanchez <prince.sanchez@ibm.com>
2024-10-16 20:49:27 -0700

    Palette flyout search id name change
    Signed-off-by: Prince Sanchez <prince.sanchez@ibm.com>

--- canvas_modules/common-canvas/src/palette/palette-flyout-content-search.jsx
+++ canvas_modules/common-canvas/src/palette/palette-flyout-content-search.jsx
@@ -50,9 +50,9 @@ class PaletteFlyoutContentSearch extends React.Component {
 
 		return (
 			// palette-flyout-search id added for hopscotch tours
-			<div className="palette-flyout-search-container" id="palette-flyout-search">
+			<div className="palette-flyout-search-container" id="palette-flyout-search-container">
 				<Search
-					id="palette-flyout-search-input"
+					id="palette-flyout-search"
 					className="palette-flyout-search"
 					placeholder={placeHolder}
 					onChange={this.handleSearchStringChange}
