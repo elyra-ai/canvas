4ff78e824ff212785da29f011f94ab98491e93a8
Prince Sanchez <prince.sanchez@ibm.com>
2024-10-29 11:59:35 -0700

    Changed fontFamily to font-family
    Signed-off-by: Prince Sanchez <prince.sanchez@ibm.com>

--- canvas_modules/harness/cypress/e2e/canvas/comments-wysiwyg.cy.js
+++ canvas_modules/harness/cypress/e2e/canvas/comments-wysiwyg.cy.js
@@ -11,7 +11,7 @@ describe("Test for verifying WYSIWYG comments functionality in the application",
 	it("Test to verify functionality of font option in WYSIWYG toolbar", function() {
 		cy.clickTextToolbarOption("submenu-font", "IBM Plex Mono");
 		cy.clickCanvasAt(5, 5);
-		cy.verifyWysiwygCommentStyles({ styleName: "fontFamily", styleValue: "IBM Plex Mono" });
+		cy.verifyWysiwygCommentStyles({ styleName: "font-family", styleValue: "IBM Plex Mono" });
 	});
 
 	it("Test to verify functionality of text size option in WYSIWYG toolbar", function() {
