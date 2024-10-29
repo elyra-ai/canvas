57f7110df9cc371c4e2f828d0d8df0f8405b2874
PRINCESANCHEZ <67511950+PRINCESANCHEZ@users.noreply.github.com>
2024-10-29 12:22:11 -0700

    #2222 Create Cypress test cases for WYSIWYG comments (#2223)
    Signed-off-by: Prince Sanchez <prince.sanchez@ibm.com>

---
+++ canvas_modules/harness/cypress/e2e/canvas/comments-wysiwyg.cy.js
@@ -0,0 +1,76 @@
+describe("Test for verifying WYSIWYG comments functionality in the application", function() {
+	beforeEach(() => {
+		cy.visit("/");
+		cy.setCanvasConfig({ "selectedWYSIWYGComments": true });
+		cy.rightClickToDisplayContextMenu(500, 50);
+		cy.verifyOptionInContextMenu("New WYSIWYG comment");
+		cy.clickOptionFromContextMenu("New WYSIWYG comment");
+		cy.editTextInComment("", "First Comment", false);
+	});
+
+	it("Test to verify functionality of font option in WYSIWYG toolbar", function() {
+		cy.clickTextToolbarOption("submenu-font", "IBM Plex Mono");
+		cy.clickCanvasAt(5, 5);
+		cy.verifyWysiwygCommentStyles({ styleName: "font-family", styleValue: "IBM Plex Mono" });
+	});
+
+	it("Test to verify functionality of text size option in WYSIWYG toolbar", function() {
+		cy.clickTextToolbarOption("submenu-text-size", "18");
+		cy.clickCanvasAt(5, 5);
+		cy.verifyWysiwygCommentStyles({ styleName: "font-size", styleValue: "18px" });
+	});
+
+	it("Test to verify functionality of bold option in WYSIWYG toolbar", function() {
+		cy.clickTextToolbarOption("bold");
+		cy.clickCanvasAt(5, 5);
+		cy.verifyWysiwygCommentStyles({ styleName: "font-weight", styleValue: "600" });
+	});
+
+	it("Test to verify functionality of italics option in WYSIWYG toolbar", function() {
+		cy.clickTextToolbarOption("italics");
+		cy.clickCanvasAt(5, 5);
+		cy.verifyWysiwygCommentStyles({ styleName: "font-style", styleValue: "italic" });
+	});
+
+	it("Test to verify functionality of underline option in WYSIWYG toolbar", function() {
+		cy.clickTextToolbarOption("underline");
+		cy.clickCanvasAt(5, 5);
+		cy.verifyWysiwygCommentStyles({ styleName: "text-decoration", styleValue: "underline" });
+	});
+
+	it("Test to verify functionality of striketrough option in WYSIWYG toolbar", function() {
+		cy.clickTextToolbarOption("strikethrough");
+		cy.clickCanvasAt(5, 5);
+		cy.verifyWysiwygCommentStyles({ styleName: "text-decoration", styleValue: "line-through" });
+	});
+
+	it("Test to verify functionality of text color option in WYSIWYG toolbar", function() {
+		cy.clickTextToolbarOption("submenu-text-color", "#A2191F");
+		cy.clickCanvasAt(5, 5);
+		cy.verifyWysiwygCommentStyles({ styleName: "color", styleValue: "rgb(162, 25, 31)" });
+	});
+
+	it("Test to verify functionality of horizontal align option in WYSIWYG toolbar", function() {
+		cy.clickTextToolbarOption("submenu-align-horiz", "Center");
+		cy.clickCanvasAt(5, 5);
+		cy.verifyWysiwygCommentStyles({ styleName: "text-align", styleValue: "center" });
+	});
+
+	it("Test to verify functionality of vertical align option in WYSIWYG toolbar", function() {
+		cy.clickTextToolbarOption("submenu-align-vert", "Bottom");
+		cy.clickCanvasAt(5, 5);
+		cy.verifyWysiwygCommentStyles({ styleName: "vertical-align", styleValue: "bottom" });
+	});
+
+	it("Test to verify functionality of background color option in WYSIWYG toolbar", function() {
+		cy.clickTextToolbarOption("submenu-background-color", "#0E6027");
+		cy.clickCanvasAt(5, 5);
+		cy.verifyWysiwygCommentStyles({ styleName: "background-color", styleValue: "rgb(14, 96, 39)" });
+	});
+
+	it("Test to verify functionality of outline option in WYSIWYG toolbar", function() {
+		cy.clickTextToolbarOption("sub-menu-outline", "Solid outline");
+		cy.clickCanvasAt(5, 5);
+		cy.verifyWysiwygCommentStyles({ styleName: "border-width", styleValue: "1px" });
+	});
+});
