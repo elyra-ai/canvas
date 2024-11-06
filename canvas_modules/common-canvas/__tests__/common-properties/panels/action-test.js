5d17bcddc8bb5d2575a97d1eb23f970dc16bef14
Jerinjk14 <Jerin.George14@ibm.com>
2024-11-07 01:00:11 +0530

    #2235 Rtl panel migration-Part1 (#2217)

--- canvas_modules/common-canvas/__tests__/common-properties/panels/action-test.js
+++ canvas_modules/common-canvas/__tests__/common-properties/panels/action-test.js
@@ -13,84 +13,100 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+import { fireEvent, waitFor, cleanup } from "@testing-library/react";
 import { expect } from "chai";
-import propertyUtils from "./../../_utils_/property-utils";
+import propertyUtilsRTL from "../../_utils_/property-utilsRTL";
 import panelConditionsParamDef from "./../../test_resources/paramDefs/panelConditions_paramDef.json";
 
 describe("action panel visible and enabled conditions work correctly", () => {
 	let wrapper;
 	let controller;
 	beforeEach(() => {
-		const renderedObject = propertyUtils.flyoutEditorForm(panelConditionsParamDef);
+		const renderedObject = propertyUtilsRTL.flyoutEditorForm(panelConditionsParamDef);
 		wrapper = renderedObject.wrapper;
 		controller = renderedObject.controller;
 	});
 
 	afterEach(() => {
-		wrapper.unmount();
+		cleanup();
 	});
 
 	it("action panels should be hidden", () => {
-		const category = wrapper.find("div[data-id='properties-action-panels']");
-		const hiddenCheckbox = category.find("div[data-id='properties-actionHide'] input");
-		expect(hiddenCheckbox.props().checked).to.equal(false);
-		expect(wrapper.find("div.properties-action-panel .hide")).to.have.length(0);
+		const { container } = wrapper;
+		const category = container.querySelector("div[data-id='properties-action-panels']");
+		const hiddenCheckbox = category.querySelector("div[data-id='properties-actionHide'] input");
+		expect(hiddenCheckbox.checked).to.equal(false);
+		expect(container.querySelectorAll("div.properties-action-panel .hide")).to.have.length(0);
+
 		// hide action panel
-		hiddenCheckbox.getDOMNode().checked = true;
-		hiddenCheckbox.simulate("change");
+		fireEvent.click(hiddenCheckbox);
+
 		// action panel should be hidden "hidden"
 		expect(controller.getPanelState({ name: "action-buttons-panel" })).to.equal("hidden");
-		expect(wrapper.find("div.properties-action-panel.hide")).to.have.length(1);
-	});
-	it("action panels should be disabled", () => {
-		const category = wrapper.find("div[data-id='properties-action-panels']");
-		const disableCheckbox = category.find("div[data-id='properties-actionDisable'] input");
-		expect(disableCheckbox.props().checked).to.equal(false);
-		expect(wrapper.find("div[data-id='properties-action-buttons-panel']").prop("disabled")).to.equal(false);
-		// hide action panel
-		disableCheckbox.getDOMNode().checked = true;
-		disableCheckbox.simulate("change");
+		expect(container.querySelectorAll("div.properties-action-panel.hide")).to.have.length(1);
 
-		// action panel should be hidden "hidden"
-		expect(controller.getPanelState({ name: "action-buttons-panel" })).to.equal("disabled");
-		expect(wrapper.find("div[data-id='properties-action-buttons-panel']").prop("disabled")).to.equal(true);
+
 	});
-	it("action panels should have label and description", () => {
-		const actionPanel = wrapper.find("div[data-id='properties-action-buttons-panel']");
-		const labelContainer = actionPanel.find(".properties-label-container");
+	it("action panels should be disabled", async() => {
+		const { container } = wrapper;
+		const category = container.querySelector("div[data-id='properties-action-panels']");
+		const disableCheckbox = category.querySelector("div[data-id='properties-actionDisable'] input");
+		expect(disableCheckbox.checked).to.equal(false);
+		const actionPanel = container.querySelector("div[data-id='properties-action-buttons-panel']");
+		expect(actionPanel.classList.contains("disabled")).to.equal(false);
+		// hide action panel
+		fireEvent.click(disableCheckbox);
+		expect(disableCheckbox.checked).to.equal(true);
+		// action panel should be hidden "hidden"
+		await waitFor(() => {
+			const actionPanelButton = container.querySelector("div[data-id='properties-action-buttons-panel']");
+			expect(controller.getPanelState({ name: "action-buttons-panel" })).to.equal("disabled");
+			expect(actionPanelButton.classList.contains("disabled"));
+		});
+
+	});
+	it("action panels should have label and description", async() => {
+		const { getByText, container } = wrapper;
+		const actionPanel = container.querySelector("div[data-id='properties-action-buttons-panel']");
+		const labelContainer = actionPanel.querySelectorAll(".properties-label-container");
 		expect(labelContainer).to.have.length(1);
+		const label = labelContainer[0].querySelector("label.properties-control-label");
 		// Verify label
-		expect(labelContainer.find("label.properties-control-label").text()).to.equal("Action panel label");
+		expect(label.textContent).to.equal("Action panel label");
 
 		// Verify description in tooltip
-		const tooltip = labelContainer.find("div.tooltip-container");
+		const tooltip = labelContainer[0].querySelectorAll("div.tooltip-container");
 		expect(tooltip).to.have.length(1);
 		// tooltip icon
-		expect(tooltip.find("svg.canvas-state-icon-information-hollow")).to.have.length(1);
+		const tooltipIcon = tooltip[0].querySelectorAll("svg.canvas-state-icon-information-hollow");
+		expect(tooltipIcon).to.have.length(1);
 		// tooltip text
-		expect(tooltip.find("div.common-canvas-tooltip span").text()).to.equal("Action panel description");
+		const tooltipText = getByText(/Action Panel description/i);
+		expect(tooltipText).to.exist;
+
 	});
 });
 
 describe("action panel classNames applied correctly", () => {
 	let wrapper;
 	beforeEach(() => {
-		const renderedObject = propertyUtils.flyoutEditorForm(panelConditionsParamDef);
+		const renderedObject = propertyUtilsRTL.flyoutEditorForm(panelConditionsParamDef);
 		wrapper = renderedObject.wrapper;
 	});
 
 	afterEach(() => {
-		wrapper.unmount();
+		cleanup();
 	});
 
 	it("action panel should have custom classname defined", () => {
-		const actionPanelWrapper = wrapper.find("div[data-id='properties-action-panels']");
-		expect(actionPanelWrapper.find(".action-buttons-panel-group-actionpanel-class")).to.have.length(1);
+		const { container } = wrapper;
+		const actionPanelWrapper = container.querySelector("div[data-id='properties-action-panels']");
+		expect(actionPanelWrapper.querySelectorAll(".action-buttons-panel-group-actionpanel-class")).to.have.length(1);
 	});
 
 	it("nested action panel should have custom classname defined", () => {
-		const panelsWrapper = wrapper.find("div[data-id='properties-panels-in-panels']");
-		expect(panelsWrapper.find(".disable-button-action-panel-group-actionpanel-class")).to.have.length(1);
+		const { container } = wrapper;
+		const panelsWrapper = container.querySelector("div[data-id='properties-panels-in-panels']");
+		expect(panelsWrapper.querySelectorAll(".disable-button-action-panel-group-actionpanel-class")).to.have.length(1);
 	});
 });
