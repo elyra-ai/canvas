58a031a47af3d3d1a9a95b8a39e45520b9e5f371
srikant <srksriks123@gmail.com>
2025-01-31 21:44:16 +0530

    Fix textarea control
    Signed-off-by: srikant <srksriks123@gmail.com>

--- canvas_modules/common-canvas/src/common-properties/controls/textarea/textarea.jsx
+++ canvas_modules/common-canvas/src/common-properties/controls/textarea/textarea.jsx
@@ -78,7 +78,7 @@ class TextareaControl extends React.Component {
 					placeholder={this.props.control.additionalText}
 					value={value}
 					labelText={this.props.controlItem}
-					hideLabel={!this.props.control.labelVisible || this.props.tableControl}
+					hideLabel={this.props.tableControl}
 					helperText={this.props.control.helperText}
 					readOnly={this.props.readOnly}
 					aria-label={this.props.control.labelVisible ? null : this.props.control?.label?.text}
@@ -95,7 +95,7 @@ class TextareaControl extends React.Component {
 					onChange={this.handleChange.bind(this)}
 					value={value}
 					labelText={this.props.controlItem}
-					hideLabel={!this.props.control.labelVisible || this.props.tableControl}
+					hideLabel={this.props.tableControl}
 					helperText={this.props.control.helperText}
 					readOnly={this.props.readOnly}
 					aria-label={this.props.control.labelVisible ? null : this.props.control?.label?.text}
