e816065dec4d8ca2b45504df5d3fe61267366e10
CTomlyn <tomlyn@us.ibm.com>
2025-03-04 21:47:28 -0800

    Temp changes6
    Signed-off-by: CTomlyn <tomlyn@us.ibm.com>

--- canvas_modules/harness/cypress/e2e/canvas/node-drag-without-select.cy.js
+++ canvas_modules/harness/cypress/e2e/canvas/node-drag-without-select.cy.js
@@ -34,6 +34,7 @@ describe("Test node operations when enableDragWithoutSelect is true", function()
 		cy.verifyNumberOfSelectedObjects(1);
 
 		cy.ctrlOrCmdClickNode("Binding (entry) node");
+		cy.wait(50);
 		cy.verifyNumberOfSelectedObjects(2);
 		cy.verifyNodeIsSelected("Super node");
 		cy.verifyNodeIsSelected("Binding (entry) node");
