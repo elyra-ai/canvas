c2cdc1614fe1223399a6bbf4ac70a7cf7e854598
srikant <srksriks123@gmail.com>
2025-05-20 16:12:26 +0530

    revert changes
    Signed-off-by: srikant <srksriks123@gmail.com>

--- canvas_modules/common-canvas/src/toolbar/toolbar-button-item.jsx
+++ canvas_modules/common-canvas/src/toolbar/toolbar-button-item.jsx
@@ -313,7 +313,7 @@ class ToolbarButtonItem extends React.Component {
 			const direction = this.props.tooltipDirection ? this.props.tooltipDirection : "bottom";
 
 			return (
-				<Tooltip id={tooltipId} tip={tip} disable={!enableTooltip} className="icon-tooltip" direction={direction} hoverable={this.props.actionObj.hoverable}>
+				<Tooltip id={tooltipId} tip={tip} disable={!enableTooltip} className="icon-tooltip" direction={direction} hoverable>
 					{content}
 				</Tooltip>
 			);
@@ -380,8 +380,7 @@ ToolbarButtonItem.propTypes = {
 			PropTypes.string,
 			PropTypes.object,
 			PropTypes.func
-		]),
-		hoverable: PropTypes.bool,
+		])
 	}),
 	actionName: PropTypes.string.isRequired,
 	tooltipDirection: PropTypes.oneOf(["top", "bottom"]),
