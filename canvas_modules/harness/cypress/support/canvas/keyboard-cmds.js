b2c54c299d823128abd38dc2353382c92779d162
CTomlyn <tomlyn@us.ibm.com>
2025-05-23 18:30:57 -0700

    Fix cypress
    Signed-off-by: CTomlyn <tomlyn@us.ibm.com>

--- canvas_modules/harness/cypress/support/canvas/keyboard-cmds.js
+++ canvas_modules/harness/cypress/support/canvas/keyboard-cmds.js
@@ -82,11 +82,11 @@ Cypress.Commands.add("shortcutKeysMarkdown", (action) => {
 	cy.useCtrlOrCmdKey().then((cmndCtrlKey) => {
 		switch (action) {
 		case "increaseHashes": {
-			cy.get("body").type(cmndCtrlKey + "{>}");
+			cy.get("body").type(cmndCtrlKey + "{rightArrow}");
 			break;
 		}
 		case "decreaseHashes": {
-			cy.get("body").type(cmndCtrlKey + "{<}");
+			cy.get("body").type(cmndCtrlKey + "{leftArrow}");
 			break;
 		}
 		case "bold": {
