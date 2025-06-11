21194eaa76e9fe6355b3f0b3bc0e367e80240558
srikant <srksriks123@gmail.com>
2025-06-11 21:58:11 +0530

    Include package.json changes
    Signed-off-by: srikant <srksriks123@gmail.com>

--- canvas_modules/common-canvas/src/common-properties/controls/datepicker/datepicker.jsx
+++ canvas_modules/common-canvas/src/common-properties/controls/datepicker/datepicker.jsx
@@ -82,8 +82,7 @@ class DatepickerControl extends React.Component {
 					onChange={this.handleChange.bind(this)}
 					locale={this.locale}
 					readOnly={this.props.readOnly}
-					// Check build
-					// value={this.state.value}
+					value={this.state.value}
 				>
 					<DatePickerInput
 						{...validationProps}
