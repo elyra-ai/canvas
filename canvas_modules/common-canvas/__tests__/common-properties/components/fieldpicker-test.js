ac9da98c1e2005e8f102c83be9d8958937b19f4e
Carita Ou <cou@us.ibm.com>
2025-10-01 17:19:11 -0700

    update carbon react and testing libraries
    Signed-off-by: Carita Ou <cou@us.ibm.com>

--- canvas_modules/common-canvas/__tests__/common-properties/components/fieldpicker-test.js
+++ canvas_modules/common-canvas/__tests__/common-properties/components/fieldpicker-test.js
@@ -349,7 +349,7 @@ describe("field-picker-control renders correctly", () => {
 		const { container } = wrapper;
 
 		const searchContainer = container.querySelector("div.properties-ft-search-container");
-		const input = searchContainer.querySelector("input[type='text']");
+		const input = searchContainer.querySelector("input");
 		fireEvent.change(input, { target: { value: "Time" } });
 		expect(tableUtilsRTL.getTableRows(container)).to.have.length(2);
 		// test case insensitive
@@ -495,7 +495,7 @@ describe("field-picker-control with multi input schemas renders correctly", () =
 		expect(tableRows.length).to.equal(29);
 
 		const searchContainer = fieldpicker.querySelector("div.properties-ft-search-container");
-		const input = searchContainer.querySelector("input[type='text']");
+		const input = searchContainer.querySelector("input");
 		fireEvent.change(input, { target: { value: "Time" } });
 
 		fieldpicker = container.querySelector("div.properties-fp-table");
@@ -549,7 +549,7 @@ describe("field-picker-control with multi input schemas renders correctly", () =
 		const { container } = wrapper;
 		let fieldpicker = tableUtilsRTL.openFieldPicker(container, "properties-ft-structuretableMultiInputSchema");
 		const searchContainer = fieldpicker.querySelector("div.properties-ft-search-container");
-		const input = searchContainer.querySelector("input[type='text']");
+		const input = searchContainer.querySelector("input");
 		fireEvent.change(input, { target: { value: "time" } });
 
 		clickFilter(wrapper, "time");
@@ -676,7 +676,7 @@ describe("field-picker-control with multi input schemas renders correctly", () =
 	it("should be able to select the same field name from different schemas", () => {
 		const { container } = wrapper;
 		let fieldpicker = tableUtilsRTL.openFieldPicker(container, "properties-ft-structuretableMultiInputSchema");
-		const search = fieldpicker.querySelector("div.properties-ft-search-container").querySelector("input[type='text']");
+		const search = fieldpicker.querySelector("div.properties-ft-search-container").querySelector("input");
 		fireEvent.change(search, { target: { value: "age" } });
 		fieldpicker = tableUtilsRTL.openFieldPicker(container, "properties-ft-structuretableMultiInputSchema");
 
