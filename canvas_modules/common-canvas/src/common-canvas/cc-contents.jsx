12ca8d59f09ce038eeed007597f30af4e0ff237a
CTomlyn <tomlyn@us.ibm.com>
2026-01-13 15:02:40 -0800

    #2844 When updating the toolbar, the application focuses on the canvas even if you are focused on an input in the right flyout.
    Signed-off-by: CTomlyn <tomlyn@us.ibm.com>

--- canvas_modules/common-canvas/src/common-canvas/cc-contents.jsx
+++ canvas_modules/common-canvas/src/common-canvas/cc-contents.jsx
@@ -522,7 +522,9 @@ class CanvasContents extends React.Component {
 		}
 	}
 
-	// Returns true if the target element passed in is inside the canvas div.
+	// Returns true if the target element passed in is either the canvas div
+	// or is inside the canvas div.
+	// Utility function called from canvas-controller.js.
 	isTargetInsideCanvas(target) {
 		return target && target.closest(".common-canvas-drop-div");
 	}
