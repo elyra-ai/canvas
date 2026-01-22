7ab1e6df7f040ef4aa8c087a62e3c9fbf5168652
CTomlyn <tomlyn@us.ibm.com>
2026-01-22 10:54:51 -0800

    #2850 Users are getting ReferenceError: exports is not defined (part 2)
    Signed-off-by: CTomlyn <tomlyn@us.ibm.com>

--- canvas_modules/common-canvas/locales/toolbar/locales/index.js
+++ canvas_modules/common-canvas/locales/toolbar/locales/index.js
@@ -13,18 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import de from "./de.json" with { type: "json" };
-import en from "./en.json" with { type: "json" };
-import eo from "./eo.json" with { type: "json" };
-import es from "./es.json" with { type: "json" };
-import fr from "./fr.json" with { type: "json" };
-import itIT from "./it.json" with { type: "json" };
-import ja from "./ja.json" with { type: "json" };
-import ko from "./ko.json" with { type: "json" };
-import ptBR from "./pt-BR.json" with { type: "json" };
-import sv from "./sv.json" with { type: "json" };
-import zhCN from "./zh-CN.json" with { type: "json" };
-import zhTW from "./zh-TW.json" with { type: "json" };
+import de from "./de.json";
+import en from "./en.json";
+import eo from "./eo.json";
+import es from "./es.json";
+import fr from "./fr.json";
+import itIT from "./it.json";
+import ja from "./ja.json";
+import ko from "./ko.json";
+import ptBR from "./pt-BR.json";
+import sv from "./sv.json";
+import zhCN from "./zh-CN.json";
+import zhTW from "./zh-TW.json";
 
 export default {
 	"de": de,
