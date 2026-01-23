74c329cadc212641e70f0ee0fe5ee1df618e7f7b
CTomlyn <tomlyn@us.ibm.com>
2026-01-23 11:25:31 -0800

    #2850 Users are getting ReferenceError: exports is not defined (part 4) (#2854)
    Signed-off-by: CTomlyn <tomlyn@us.ibm.com>

--- canvas_modules/harness/src/intl/locales/index.js
+++ canvas_modules/harness/src/intl/locales/index.js
@@ -14,18 +14,18 @@
  * limitations under the License.
  */
 
-import de from "./de.json";
-import en from "./en.json";
-import es from "./es.json";
-import fr from "./fr.json";
-import itIT from "./it.json";
-import ja from "./ja.json";
-import ptBR from "./pt-BR.json";
-import zhTW from "./zh-TW.json";
-import zhCN from "./zh-CN.json";
-import eo from "./eo.json";
-import ko from "./ko.json";
-import sv from "./sv.json";
+import de from "./de.json" with { type: "json" };
+import en from "./en.json" with { type: "json" };
+import es from "./es.json" with { type: "json" };
+import fr from "./fr.json" with { type: "json" };
+import itIT from "./it.json" with { type: "json" };
+import ja from "./ja.json" with { type: "json" };
+import ptBR from "./pt-BR.json" with { type: "json" };
+import zhTW from "./zh-TW.json" with { type: "json" };
+import zhCN from "./zh-CN.json" with { type: "json" };
+import eo from "./eo.json" with { type: "json" };
+import ko from "./ko.json" with { type: "json" };
+import sv from "./sv.json" with { type: "json" };
 
 export default {
 	"de": de,
