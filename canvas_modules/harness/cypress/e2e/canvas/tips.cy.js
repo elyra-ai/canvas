d762935523b9cf509cddb67fb09b7ecb19e60d96
CTomlyn <tomlyn@us.ibm.com>
2026-03-05 17:55:24 -0800

    Copyrights
    Signed-off-by: CTomlyn <tomlyn@us.ibm.com>

--- canvas_modules/harness/cypress/e2e/canvas/tips.cy.js
+++ canvas_modules/harness/cypress/e2e/canvas/tips.cy.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2023 Elyra Authors
+ * Copyright 2017-2026 Elyra Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -661,5 +661,3 @@ describe("Test undo redo tooltips for different actions", function() {
 			cy.clickToolbarRedo();
 		});
 });
-
-
