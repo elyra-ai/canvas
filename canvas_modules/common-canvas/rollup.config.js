ef117a57476199018e7828ddd1666f673b599901
CTomlyn <tomlyn@us.ibm.com>
2026-03-19 13:10:02 -0700

    change rollup instead
    Signed-off-by: CTomlyn <tomlyn@us.ibm.com>

--- canvas_modules/common-canvas/rollup.config.js
+++ canvas_modules/common-canvas/rollup.config.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2023 Elyra Authors
+ * Copyright 2017-2026 Elyra Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -77,7 +77,14 @@ export default {
 				"@babel/plugin-proposal-class-properties",
 				"@babel/plugin-transform-runtime",
 				"@babel/plugin-proposal-export-default-from",
-				["transform-react-remove-prop-types", { removeImport: true }]
+				["transform-react-remove-prop-types", { removeImport: true }],
+				["transform-imports", {
+					"@carbon/react/icons": {
+						"transform": "@carbon/icons-react/lib/${member}",
+						"preventFullImport": true,
+						"skipDefaultConversion": true
+					}
+				}]
 			]
 		}),
 		terser(),
