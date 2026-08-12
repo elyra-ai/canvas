8a3e5b3e132ee00900b7d745a2eaf3b5ec638c26
Amelia <amelialx99@gmail.com>
2026-08-11 19:41:35 -0500

    Add aria-disabled to disabled context menu items
    Mark disabled menu items with aria-disabled={true}
    
    Signed-off-by: Amelia <amelialx99@gmail.com>

--- canvas_modules/common-canvas/src/context-menu/common-context-menu.jsx
+++ canvas_modules/common-canvas/src/context-menu/common-context-menu.jsx
@@ -276,7 +276,7 @@ class CommonContextMenu extends React.Component {
 					menuRefs.push(ref);
 
 					menuItem = (
-						<div key={i} ref={ref} tabIndex={0} className={"context-menu-item disabled"} onKeyDown={this.onKeyDown} role="menuitem">
+						<div key={i} ref={ref} tabIndex={0} className={"context-menu-item disabled"} onKeyDown={this.onKeyDown} role="menuitem" aria-disabled={true}>
 							{menuDefinition[i].label}
 						</div>
 					);
@@ -304,7 +304,7 @@ class CommonContextMenu extends React.Component {
 
 				} else if (menuDefinition[i].enable === false) {
 					menuItem = (
-						<div key={i} className={"context-menu-item disabled"} role="menuitem">
+						<div key={i} className={"context-menu-item disabled"} role="menuitem" aria-disabled={true}>
 							{menuDefinition[i].label}
 						</div>
 					);
@@ -386,7 +386,7 @@ class CommonContextMenu extends React.Component {
 		this.subMenuPosData[menuItem.action] = subMenuPosStyle;
 
 		return (
-			<div key={index} ref={ref} className={menuItemClass} aria-haspopup tabIndex={itemTabIndex} data-action={menuItem.action} role="menuitem"
+			<div key={index} ref={ref} className={menuItemClass} aria-haspopup tabIndex={itemTabIndex} data-action={menuItem.action} role="menuitem" aria-disabled={disabled}
 				onMouseEnter={onMouseEnter}
 				onMouseLeave={onMouseLeave}
 				onKeyDown={this.onKeyDown}
