882f903160e0e12fc9dcd675f4ab60ec7150536f
CTomlyn <tomlyn@us.ibm.com>
2026-09-18 10:46:05 -0700

    CSS changes
    Signed-off-by: CTomlyn <tomlyn@us.ibm.com>

--- canvas_modules/common-canvas/src/common-properties/controls/dropdown/dropdown.jsx
+++ canvas_modules/common-canvas/src/common-properties/controls/dropdown/dropdown.jsx
@@ -307,6 +307,7 @@ class DropDown extends React.Component {
 					onInputChange={(evt) => this.handleOnInputChange(evt, dropDown.options)}
 					translateWithId={(id) => listBoxMenuIconTranslationIds[id]}
 					titleText=""
+					size="md"
 					helperText={this.props.control.helperText}
 					allowCustomValue={this.props.control.customValueAllowed}
 					{...shouldFilterItem}
