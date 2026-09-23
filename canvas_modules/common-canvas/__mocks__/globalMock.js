44db4543428c863b586fdc2eeb99f19422f13f79
CTomlyn <tomlyn@us.ibm.com>
2026-09-23 08:40:41 -0700

    Remove directive
    Signed-off-by: CTomlyn <tomlyn@us.ibm.com>

--- canvas_modules/common-canvas/__mocks__/globalMock.js
+++ canvas_modules/common-canvas/__mocks__/globalMock.js
@@ -25,9 +25,6 @@ const createContextualFragment = (html) => {
 
 global.Range.prototype.createContextualFragment = (html) => createContextualFragment(html);
 
-// Enable Carbon v12 dynamic floating styles feature flag before any modules load
-process.env.CARBON_ENABLE_V12_DYNAMIC_FLOATING_STYLES = "true";
-
 // HACK: Polyfil that allows codemirror to render in a JSDOM env.
 global.window.document.createRange = function createRange() {
 	return {
